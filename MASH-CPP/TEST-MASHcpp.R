e3d4dc0a921723a25a0e04871119e2b0c351de00
slwu89 <slwu89@berkeley.edu>
2017-08-18 04:27:22 -0700

    init MASH-MACRO

--- MASH-CPP/TEST-MASHcpp.R
+++ MASH-CPP/TEST-MASHcpp.R
@@ -34,3 +34,30 @@ myClass$queue$get_EventQ()
 myClass$eraseQueue()
 gc()
 
+# list vs. pairlist comparison
+
+testList = function(N){
+  xx = list()
+  for(i in 1:N){
+    xx[[i]] = list(numbers=1:100)
+  }
+  rm(xx)
+}
+
+testPairList = function(N){
+  xx = pairlist()
+  for(i in 1:N){
+    xx[[i]] = list(numbers=1:100)
+  }
+  rm(xx)
+}
+
+# dataPts = NULL
+for(N in c(1e1,1e2,1e3,1e4,1e5)){
+  print(microbenchmark::microbenchmark(
+    testList(N = N),
+    testPairList(N = N),
+    times = 100
+  ))
+}
+
