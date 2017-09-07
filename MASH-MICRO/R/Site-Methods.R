9133603b0d98179b94b0863526a9233938586910
slwu89 <slwu89@berkeley.edu>
2017-09-06 20:06:01 -0700

    Feeding site methods

--- MASH-MICRO/R/Site-Methods.R
+++ MASH-MICRO/R/Site-Methods.R
@@ -35,10 +35,10 @@ FeedingSite$set(which = "public",name = "get_ix",
 #'
 #' @param ix integer
 #'
-set_ix <- function(ix){private$ix = ix}
+set_ix_FeedingSite <- function(ix){private$ix = ix}
 
 FeedingSite$set(which = "public",name = "get_ix",
-  value = get_ix_FeedingSite, overwrite = TRUE
+  value = set_ix_FeedingSite, overwrite = TRUE
 )
 
 
@@ -47,7 +47,11 @@ FeedingSite$set(which = "public",name = "get_ix",
 #' Get site coordinates \code{siteXY}
 #'  * This method is bound to \code{FeedingSite$get_siteXY}
 #'
-get_siteXY <- function(){return(private$siteXY)}
+get_siteXY_FeedingSite <- function(){return(private$siteXY)}
+
+FeedingSite$set(which = "public",name = "get_siteXY",
+  value = get_siteXY_FeedingSite, overwrite = TRUE
+)
 
 #' Set siteXY
 #'
@@ -56,14 +60,22 @@ get_siteXY <- function(){return(private$siteXY)}
 #'
 #' @param ix numeric(2)
 #'
-set_siteXY <- function(newSiteXY){private$siteXY = siteXY}
+set_siteXY_FeedingSite <- function(newSiteXY){private$siteXY = set_siteXY}
+
+FeedingSite$set(which = "public",name = "get_siteXY",
+  value = set_siteXY_FeedingSite, overwrite = TRUE
+)
 
 #' Get searchWt
 #'
 #' Get site search weight \code{searchWt}
 #'  * This method is bound to \code{FeedingSite$get_searchWt}
 #'
-get_searchWt <- function(){return(private$searchWt)}
+get_searchWt_FeedingSite <- function(){return(private$get_searchWt)}
+
+FeedingSite$set(which = "public",name = "get_siteXY",
+  value = get_searchWt_FeedingSite, overwrite = TRUE
+)
 
 #' Set searchWt
 #'
@@ -72,14 +84,23 @@ get_searchWt <- function(){return(private$searchWt)}
 #'
 #' @param ix numeric
 #'
-set_searchWt <- function(searchWt){private$searchWt = searchWt}
+set_searchWt_FeedingSite <- function(searchWt){private$searchWt = searchWt}
+
+FeedingSite$set(which = "public",name = "set_searchWt",
+  value = set_searchWt_FeedingSite, overwrite = TRUE
+)
+
 
 #' Get siteType
 #'
 #' Get site type \code{siteType}; 1 is peri-domestic, 0 is non peri-domestic.
 #'  * This method is bound to \code{FeedingSite$get_siteType}
 #'
-get_siteType <- function(){return(private$siteType)}
+get_siteType_FeedingSite <- function(){return(private$siteType)}
+
+FeedingSite$set(which = "public",name = "get_siteType",
+  value = get_siteType_FeedingSite, overwrite = TRUE
+)
 
 #' Set siteType
 #'
@@ -88,23 +109,92 @@ get_siteType <- function(){return(private$siteType)}
 #'
 #' @param siteType integer
 #'
-set_siteType <- function(siteType){priate$siteType = siteType}
+set_siteType_FeedingSite <- function(siteType){priate$siteType = siteType}
+
+FeedingSite$set(which = "public",name = "set_siteType",
+  value = set_siteType_FeedingSite, overwrite = TRUE
+)
 
 
-# vegetation hazards
-get_hazV <- function(){return(private$hazV)}
-set_hazV <- function(hazV){private$hazV = hazV}
+#' Get hazV
+#'
+#' Get landing hazard associated with vegetation at this site \code{hazV}
+#'  * This method is bound to \code{FeedingSite$get_hazV}
+#'
+get_hazV_FeedingSite <- function(){return(private$hazV)}
 
-# outside wall hazards
-get_hazW <- function(){return(private$hazW)}
-set_hazW <- function(hazW){private$hazW = hazW}
+FeedingSite$set(which = "public",name = "get_hazV",
+  value = get_hazV_FeedingSite, overwrite = TRUE
+)
 
-# inside wall hazards
-get_hazI <- function(){return(private$hazI)}
-set_hazI <- function(hazI){private$hazI = hazI}
+#' Set hazV
+#'
+#' Set landing hazard associated with vegetation at this site \code{hazV}
+#'  * This method is bound to \code{FeedingSite$set_hazV}
+#'
+#' @param hazV numeric
+#'
+set_hazV_FeedingSite <- function(hazV){private$hazV = hazV}
 
-# get hazard based on lspot
-get_hazLspot <- function(lspot){
+FeedingSite$set(which = "public",name = "set_hazV",
+  value = set_hazV_FeedingSite, overwrite = TRUE
+)
+
+
+#' Get hazW
+#'
+#' Get landing hazard associated with outside wall at this site \code{get_hazW}
+#'  * This method is bound to \code{FeedingSite$get_hazW}
+#'
+get_hazW_FeedingSite <- function(){return(private$hazW)}
+
+FeedingSite$set(which = "public",name = "get_hazW",
+  value = get_hazW_FeedingSite, overwrite = TRUE
+)
+
+#' Set hazW
+#'
+#' Set landing hazard associated with outside wall at this site \code{hazW}
+#'  * This method is bound to \code{FeedingSite$set_hazW}
+#'
+#' @param hazW numeric
+#'
+set_hazW_FeedingSite <- function(hazW){private$hazW = hazW}
+
+FeedingSite$set(which = "public",name = "set_hazW",
+  value = set_hazW_FeedingSite, overwrite = TRUE
+)
+
+#' Get hazI
+#'
+#' Get landing hazard associated with indoor wall at this site \code{hazI}
+#'  * This method is bound to \code{FeedingSite$get_hazI}
+#'
+get_hazI_FeedingSite <- function(){return(private$hazI)}
+
+FeedingSite$set(which = "public",name = "get_hazI",
+  value = get_hazI_FeedingSite, overwrite = TRUE
+)
+
+#' Set hazI
+#'
+#' Set landing hazard associated with indoor wall at this site \code{hazI}
+#'  * This method is bound to \code{FeedingSite$set_hazI}
+#'
+#' @param hazI numeric
+#'
+set_hazI_FeedingSite <- function(hazI){private$hazI = hazI}
+
+FeedingSite$set(which = "public",name = "set_hazI",
+  value = set_hazI_FeedingSite, overwrite = TRUE
+)
+
+#' Get Landing Hazards
+#'
+#' Get landing hazard associated with one of the spots a mosquito may land at for this site.
+#'  * This method is bound to \code{FeedingSite$get_hazLspot}
+#'
+get_hazLspot_FeedingSite <- function(lspot){
   switch(lspot,
      i = {return(private$hazI)},
      w = {return(private$hazW)},
@@ -114,22 +204,85 @@ get_hazLspot <- function(lspot){
    )
 }
 
-# house entry probability
-get_enterP <- function(){return(private$enterP)}
-set_enterP <- function(enterP){private$enterP = enterP}
+FeedingSite$set(which = "public",name = "get_hazLspot",
+  value = get_hazLspot_FeedingSite, overwrite = TRUE
+)
 
-# host risk queue
-get_RiskQ <- function(){return(private$RiskQ)}
-set_RiskQ <- function(RiskQ){private$RiskQ = RiskQ}
+#' Get enterP
+#'
+#' Get house entry probability at this site \code{enterP}
+#'  * This method is bound to \code{FeedingSite$get_enterP}
+#'
+get_enterP_FeedingSite <- function(){return(private$enterP)}
 
-clear_RiskQ <- function(){
+FeedingSite$set(which = "public",name = "get_enterP",
+  value = get_enterP_FeedingSite, overwrite = TRUE
+)
+
+#' Set enterP
+#'
+#' Set landing hazard associated with indoor wall at this site \code{enterP}
+#'  * This method is bound to \code{FeedingSite$set_enterP}
+#'
+#' @param enterP numeric
+#'
+set_enterP_FeedingSite <- function(enterP){private$enterP = enterP}
+
+FeedingSite$set(which = "public",name = "set_enterP",
+  value = set_enterP_FeedingSite, overwrite = TRUE
+)
+
+
+#' Get RiskQ
+#'
+#' Get the host risk queue at this site, an object of class \code{\link[MASHcpp]{RiskQ}}
+#'  * This method is bound to \code{FeedingSite$get_RiskQ}
+#'
+get_RiskQ_FeedingSite <- function(){return(private$RiskQ)}
+
+FeedingSite$set(which = "public",name = "get_RiskQ",
+  value = get_RiskQ_FeedingSite, overwrite = TRUE
+)
+
+#' Clear RiskQ
+#'
+#' Clear the human risk queue at this site, an object of class \code{\link[MASHcpp]{RiskQ}}
+#'  * This method is bound to \code{FeedingSite$clear_RiskQ}
+#'
+clear_RiskQ_FeedingSite <- function(){
   private$RiskQ$clear_HumanHost()
 }
 
+FeedingSite$set(which = "public",name = "clear_RiskQ",
+  value = clear_RiskQ_FeedingSite, overwrite = TRUE
+)
 
-# landscape pointer
-get_LandscapePointer <- function(){return(private$LandscapePointer)}
-set_LandscapePointer <- function(LandscapePointer){private$LandscapePointer = LandscapePointer}
+
+#' Get \code{\link{Landscape}} Pointer
+#'
+#' Return \code{Landscape$self} enclosing this site.
+#'  * This method is bound to \code{FeedingSite$get_LandscapePointer}
+#'
+get_LandscapePointer_FeedingSite <- function(){return(private$LandscapePointer)}
+
+FeedingSite$set(which = "public",name = "get_LandscapePointer",
+  value = get_LandscapePointer_FeedingSite, overwrite = TRUE
+)
+
+
+#' Set \code{\link{Landscape}} Pointer
+#'
+#' Set the pointer \code{Landscape$self} enclosing this site.
+#'  * This method is bound to \code{FeedingSite$set_LandscapePointer}
+#'
+#' @param LandscapePointer an environment
+#'
+set_LandscapePointer_FeedingSite <- function(LandscapePointer){private$LandscapePointer = LandscapePointer}
+
+FeedingSite$set(which = "public",name = "set_LandscapePointer",
+  value = set_LandscapePointer_FeedingSite, overwrite = TRUE
+)
+
 
 
 ###############################################################################
