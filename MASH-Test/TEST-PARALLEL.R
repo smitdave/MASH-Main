2734e895d54daa65d8f3ef428997cddbbe24327e
slwu89 <slwu89@berkeley.edu>
2017-09-14 11:45:01 -0700

    try out par

--- MASH-Test/TEST-PARALLEL.R
+++ MASH-Test/TEST-PARALLEL.R
@@ -15,13 +15,101 @@ library(parallel)
 # we use a socket cluster because we don't want to fork the current environment
 cl = parallel::makeCluster(spec = 2,type = "PSOCK")
 
-writeBin(object = list(id=1L),con = cl[[1]]$con)
+parallel::clusterEvalQ(cl = cl,expr = {
+  pid <- Sys.getpid()
+  name <- Sys.info()["nodename"]
+  str <- paste("This is R running on", name, "with PID", pid, "!")
+  return(str)
+})
 
-serialize(object = list(id=1L),connection = cl[[1]]$con)
 
-parallel::clusterEvalQ(cl = cl,expr = {ls()})
+clusterEvalQ(cl = cl,expr = {ls()})
 
-# try using parallel:::sendCall, found within clusterCall
+parallel:::sendCall(con = cl[[1]],fun = function(){
+  x = paste("This a call specifically sent to ", name, "with PID", pid, "!")
+},args = list(),return = FALSE)
 
-parallel::stopCluster(cl)
-rm(cl)
+# parallel:::sendData(node = cl[[1]],data = 5)
+
+clusterEvalQ(cl = cl,expr = {ls()})
+
+stopCluster(cl)
+
+
+
+
+
+# # parallel test
+# cl = parallel::makeCluster(spec = 2,type = "PSOCK")
+# 
+# parallel::clusterEvalQ(cl = cl,expr = {
+#   pid <- Sys.getpid()
+#   name <- Sys.info()["nodename"]
+#   str <- paste("This is R running on", name, "with PID", pid, "!")
+#   return(str)
+# })
+# 
+# parallel::clusterEvalQ(cl = cl,expr = {
+#   
+#   library(MASHmicro)
+#   # set.seed(42L)
+#   
+#   DEBUG.MASHMICRO()
+#   MASHcpp::DEBUG.MASHCPP()
+#   MASHmacro::DEBUG.MASHMACRO()
+#   
+#   # make a tile
+#   pid <- Sys.getpid()
+#   DIR = paste0("/Users/slwu89/Desktop/MASHOUT",pid,"/")
+#   
+#   # setup
+#   Humans.MICRO.Setup()
+#   PfSI.MICRO.Setup(Pf_c = 1,Pf_b = 1,LatentPf = 1,DurationPf = 20)
+#   AQUA.Emerge.Setup()
+#   
+#   MBITES.Generic.Setup()
+#   MBITES.BRO.Setup(aquaModule = "emerge",timing = "exponential")
+#   # MBITES.BRO.Cohort.Setup()
+#   
+#   SEARCH.Kernel.Setup(MBITES = "BRO")
+#   
+#   # landscape parameters
+#   nAqua = 10
+#   nFeed = 10
+#   emerge_par = list(N = nAqua,lambda = 25, lambdaWeight = NULL, offset = NULL)
+#   landscape_par = Landscape.Parameters(nFeed = nFeed,nAqua = nAqua,pointGen = "poisson",module = "emerge",modulePars = emerge_par)
+#   
+#   # human parameters
+#   human_par = MASHmacro::HumanPop.Parameters(nSite = nFeed,siteSize = 10,siteMin = 2)
+#   
+#   # M-BITES parameters
+#   nMosy = 50
+#   mbites_par = MBITES.BRO.Parameters(PfEIP=1)
+#   mosquito_par = list(
+#     N_female = nMosy,
+#     ix_female = rep(1,nMosy),
+#     genotype_female = rep(1,nMosy),
+#     MBITES_PAR = mbites_par
+#   )
+#   
+#   MicroTile = Tile$new(Landscape_PAR = landscape_par,HumanPop_PAR = human_par,MosquitoPop_PAR = mosquito_par,directory = DIR)
+#   
+#   # MicroLandscapePlot_utility(Landscape = MicroTile$get_Landscape())
+#   
+#   # MicroTile$get_FemalePop()$simCohort(N = 1e3)
+#   
+#   MicroTile$get_HumanPop()$init_ActivitySpace()
+#   
+#   # MicroTile$get_ActivitySpace()
+#   
+#   MicroTile$get_HumanPop()$init_PfSI(PfPR = 0.95)
+#   
+#   MicroTile$simMICRO_oneRun(tMax = 50,verbose = FALSE,trackPop = TRUE)
+#   
+# })
+# 
+# parallel::clusterEvalQ(cl = cl,expr = {
+#   MicroTile$get_ActivitySpace()
+# })
+# parallel::stopCluster(cl)
+# rm(cl);gc()
