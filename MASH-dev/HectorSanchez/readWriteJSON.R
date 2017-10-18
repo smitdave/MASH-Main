e38fe622153a9f9148b0d15302180190436d31d8
Hector M. Sanchez C <chipdelmal@gmail.com>
2017-10-18 16:44:26 -0700

    Corrected an error in the JSON reader

--- MASH-dev/HectorSanchez/readWriteJSON.R
+++ MASH-dev/HectorSanchez/readWriteJSON.R
@@ -1,4 +1,4 @@
 library(jsonlite)
 jsonOut=prettify(toJSON(mosquito_par))
 write(jsonOut,paste0(DIR,"mosquitoParameters.json"))
-read_json(paste0(DIR,"mosquitoParameters.json"))
+par=read_json(paste0(DIR,"mosquitoParameters.json"),simplifyVector=TRUE)
