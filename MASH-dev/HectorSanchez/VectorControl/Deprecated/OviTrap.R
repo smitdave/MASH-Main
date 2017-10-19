9ac2f78191e0c89370998e22c5780cf7e940253e
Hector M. Sanchez C <chipdelmal@gmail.com>
2017-10-18 17:50:43 -0700

    Removed VC Deprecated and added "sourceEntireFolder" function

--- MASH-dev/HectorSanchez/VectorControl/Deprecated/OviTrap.R
+++
@@ -1,9 +0,0 @@
-fOvitrap = function(M){
-  if(LANDSCAPE$aquaSites[[M$ix]]$OVITRAP){
-    if(binomialEvent(1-INTERVENTIONS_PARAMETERS$OVITRAP$SurviveP)){
-      print("Killed by ovitrap")
-      M$stateNew="D"
-    }
-  }
-  return(M)
-}
---
+++ MASH-dev/HectorSanchez/devTools.R
@@ -0,0 +1,13 @@
+sourceEntireFolder=function(folderName, verbose=FALSE, showWarnings=TRUE) {
+  files=list.files(folderName, full.names=TRUE)
+  print(files)
+  # Grab only R files
+  files=files[ grepl("\\.[rR]$", files) ]
+  if (!length(files) && showWarnings){warning("No R files in ", folderName)}
+  for (f in files) {
+    if (verbose)
+      cat("sourcing: ", f, "\n")
+    try(source(f, local=FALSE, echo=FALSE), silent=!verbose)
+  }
+  return(invisible(NULL))
+}
