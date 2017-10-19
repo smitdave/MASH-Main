6fbf818c0fb78866264b881f6ce93b002e737437
Hector M. Sanchez C <chipdelmal@gmail.com>
2017-10-18 17:11:39 -0700

    Upgrades on the new GUI

--- MASH-dev/HectorSanchez/MBITES_GUI/New/app.R
+++ MASH-dev/HectorSanchez/MBITES_GUI/New/app.R
@@ -22,8 +22,10 @@ if(system("whoami",intern=TRUE)=="chipdelmal"){
 ################################################################################################################
 # CONSTANTS
 ################################################################################################################
-INITIAL_GUI_WIDTH=1250
+INITIAL_GUI_WIDTH=2000
 INITIAL_GUI_HEIGHT=1000
+VAR_DESCR_COL_WIDTH=3
+VAR_SLIDE_COL_WIDTH=4
 THEME="flatly"
 ################################################################################################################
 mbitesGadget=function(...){
@@ -31,6 +33,7 @@ mbitesGadget=function(...){
   # UI
   #########################################################################################
   ui=shinyUI(fluidPage(theme=shinytheme(THEME),
+                       
       titlePanel(h1("MBITES Gadget")),
       navbarPage("Select an Activity: ",
           #################################################################################
@@ -41,24 +44,58 @@ mbitesGadget=function(...){
                   includeMarkdown("instructions.md"),
                   img(src='boutFull.png',align="center",width="50%")
                 ),
+                ###########################################################################
                 tabPanel("Bouts",
                   tabsetPanel(
-                     tabPanel("F","Test"),
-                     tabPanel("L","Test"),
-                     tabPanel("B","Test"),
-                     tabPanel("R","Test"),
-                     tabPanel("O","Test"),
-                     tabPanel("S","Test"),
-                     tabPanel("M","Test"),
-                     tabPanel("E","Test")
+                     ######################################################################
+                     tabPanel("F",
+                              fluidRow(h1("")),
+                              fluidRow(
+                                column(VAR_DESCR_COL_WIDTH,helpText("bfa.p: Probability of surviving a blood-feed attempt.")),
+                                column(VAR_SLIDE_COL_WIDTH,sliderInput("bfa.p",NULL,min=0,max=1,value=.9))
+                              ),
+                              fluidRow(
+                                column(VAR_DESCR_COL_WIDTH,helpText("bfa.s: Probability of success in a blood-feed attempt.")),
+                                column(VAR_SLIDE_COL_WIDTH,sliderInput("bfa.s",NULL,min=0,max=1,value=.3))
+                              ),
+                              fluidRow(
+                                column(VAR_DESCR_COL_WIDTH,helpText("bfa.t: Mean time between blood-feed attempts (days).")),
+                                column(VAR_SLIDE_COL_WIDTH,sliderInput("bfa.t",NULL,min=0,max=5,value=.65,step=.05))
+                              ),
+                              fluidRow(
+                                column(VAR_DESCR_COL_WIDTH,helpText("Q: Proportion of Bites on Humans.")),
+                                column(VAR_SLIDE_COL_WIDTH,sliderInput("Q",NULL,min=0,max=1,value=.9))
+                              ),
+                              h1(),h1()
+                      ),
+                      #####################################################################
+                      tabPanel("L","Test"),
+                      #####################################################################
+                      tabPanel("B","Test"),
+                      #####################################################################
+                      tabPanel("R","Test"),
+                      #####################################################################
+                      tabPanel("O","Test"),
+                      #####################################################################
+                      tabPanel("S","Test"),
+                      #####################################################################
+                      tabPanel("M","Test"),
+                      #####################################################################
+                      tabPanel("E","Test")
                   ),
                   h1(""),
-                  actionButton("button","An action button",width="50%"),
-                  actionButton("button","An action button",width="50%")
+                  h1(""),
+                  h1(""),
+                  tags$hr(),
+                  fluidRow(
+                    column(6,actionButton("button","Export Bouts Parameters",width="100%")),
+                    column(6,actionButton("button","Read Bouts Parameters",width="100%"))
+                  )
                 ),
+                ###########################################################################
                 tabPanel("Landscape")
               )
-          ),
+           ),
           #################################################################################
           tabPanel("Run the Model"),
           #################################################################################
