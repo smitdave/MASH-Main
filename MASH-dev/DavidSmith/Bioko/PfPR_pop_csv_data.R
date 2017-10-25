542dc92d79f34bdc666d290b535cc56f817fa282
Daniel Citron <dtcitron@uw.edu>
2017-10-25 14:44:29 -0700

    added script for reading in and plotting csv data of PfPR and population on Bioko

---
+++ MASH-dev/DavidSmith/Bioko/PfPR_pop_csv_data.R
@@ -0,0 +1,42 @@
+library(sp)
+library(raster)
+library(maptools)
+library(RColorBrewer)
+library(MASS)
+library(rgeos)
+library(GISTools)
+
+# Load in two .csv files
+# One file will contain PfPR data, indexed by areaID.  Its coordinates are the UTM projection.
+# One file will contain Population data, indexed by areaID.  Its coordinates can be written both as
+# long/lat and the UTM projection.
+
+# Set Working Directory to ~/MASH/MASH-Main/MASH-dev/DavidSmith/Bioko
+
+# Results: read in the data and create plots of how the population and PfPR are distributed on Bioko
+
+
+# This is the csv file of PfPR data
+csvPfPr <- read.csv("pfprmap_area.csv")
+head(csvPfPr)
+# force this to be a SpatialPointsDataFrame
+coordinates(csvPfPr) <- ~x+y
+
+# This is the csv file of the populated areas
+biokoPOP <- read.csv("bioko_areas.csv")
+head(biokoPOP)
+# force this to be a SpatialPointsDataFrame
+coordinates(biokoPOP) <- ~utmx+utmy
+
+# Create a plot of the inferred PfPR, and a plot of where people live
+par(mfrow=c(1,2))
+c <- csvPfPr[!is.na(csvPfPr$pr),]
+shades <- auto.shading(c$pr, n = 8, cols = brewer.pal(8,'Reds'))
+choropleth(c, c$pr, shading = shades)
+title("PfPR from CSV")
+
+c2 <- biokoPOP[!is.na(biokoPOP$pop),]
+shades <- auto.shading(c2$pop, n = 8, cols = brewer.pal(8,'Blues'))
+choropleth(c2, c2$pop, shading = shades)
+title("Population from CSV")
+
