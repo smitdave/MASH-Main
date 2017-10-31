b59014ac533ca717d0cd440ff7fe70b82f5c4d41
slwu89 <slwu89@berkeley.edu>
2017-10-31 12:29:07 -0700

    tests work

--- MASH-dev/SeanWu/MACRO/src/test.cpp
+++ MASH-dev/SeanWu/MACRO/src/test.cpp
@@ -10,11 +10,22 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 void testHuman(){
   human* h = new human(1);
-  std::cout << "my id: " << h->get_id() << std::endl;
+  std::cout << "human 1 id: " << h->get_id() << std::endl;
   h->set_state("state1");
-  std::cout << "state: " << h->get_state() << std::endl;
+  std::cout << "human 1 state: " << h->get_state() << std::endl;
   h->add2Q_set_state(1.00,"state2");
   h->fireEvent();
-  std::cout << "state: " << h->get_state() << std::endl;
+  std::cout << "human 1 state: " << h->get_state() << std::endl;
+  
+  human* h1 = new human(2);
+  std::cout << "human 2 id: " << h1->get_id() << std::endl;
+  h1->set_state("state53");
+  std::cout << "human 2 state: " << h1->get_state() << std::endl;
+  h1->add2Q_set_state(1.00,"state223");
+  h1->fireEvent();
+  std::cout << "human 2 state: " << h1->get_state() << std::endl;
+  std::cout << "human 1 state: " << h->get_state() << std::endl;
+  
   delete h;
+  delete h1;
 }
