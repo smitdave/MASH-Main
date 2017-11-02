344bafd3027f073c5391c3e98ebf72ef0823ae2c
smitdave <smitdave@ihmeadmins-MacBook-Pro-3.local>
2017-11-02 09:36:54 -0700

    Minor changes

---
+++ MASH-dev/DavidSmith/Bioko/PR2EIRlookup.R
@@ -0,0 +1,9 @@
+pr2eir = read.csv("PfEIR_to_PfPR (JUN_30).csv")
+eir = pr2eir[,1]
+pr2eir = as.matrix(pr2eir[,-1])
+treat = seq(0,1,by =0.05)
+
+filled.contour(log(eir[4:11]), treat[5:15],  pr2eir[4:11,5:15], plot.axes = {axis(1, log(c(1,4,16,64)), c(1,4,16,64) ) 
+  axis(2,treat[5:15])}, main = "PfPR", xlab = "Annual EIR", ylab = "Proportion Treated")
+
+image(pr2eir)
