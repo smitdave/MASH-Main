0551879dd75a1fe1cfd4d5ea7f659cc238b944d1
smitdave <smitdave@ihmeadmins-MacBook-Pro-3.local>
2017-11-17 10:00:04 -0800

    Minor changes.

--- MASH-dev/DavidSmith/MBITES-Demo/Demo.network.R
+++ MASH-dev/DavidSmith/MBITES-Demo/Demo.network.R
@@ -1,16 +1,16 @@
 
 kerW = function(xy, XY, w, p=1){
-  d = sqrt( (xy[,1] - XY[,1])^2 + (xy[,2] - XY[,2])^2) 
+  d = sqrt( (xy[1] - XY[,1])^2 + (xy[2] - XY[,2])^2) 
   exp(-d*p)*w
 }
 
 kerW.i = function(i, xy, XY, w, p=1){
-  kerW(xy[i,], XY, w, p)
+  kerW(xy[i,], XY, w[i], p)
 }
 
 
 LF = sapply(X=c(1:N.f), FUN=kerW.i, xy = xy.f, XY=xy.l, w=w.f, simplify = "array")
-FL = sapply(X=c(1:N.f), FUN=kerW.i, xy = xy.l, XY=xy.f,w=w.l, simplify = "array")
+FL = sapply(X=c(1:N.l), FUN=kerW.i, xy = xy.l, XY=xy.f, w=w.l, simplify = "array")
 
 LF = t(LF)
 FL = t(FL)
@@ -19,8 +19,8 @@ Q = LF%*%FL
 
 Q = Q / rowSums(Q)
 
-N = matrix(pmax(0, Q - t(Q)), 29, 29) 
-C = Q-N
+N = matrix(pmax(0, Q - t(Q)), N.f, N.f) 
+CC = Q-N
 
 Q2 = (Q %*% Q)
 Q2 = Q2 / rowSums(Q2)
@@ -28,11 +28,11 @@ Q2 = Q2 / rowSums(Q2)
 N2= matrix(pmax(0, Q2 - t(Q2)), 29, 29) 
 C2 = Q2-N2
 
-plot(xf, yf, type = "n", pch = 16, col = "blue", ylim = c(-1,12), xlim = c(-1,12), xlab = "", xaxt = "n", ylab = "", yaxt = "n")
+plot(xy.f[,1], xy.f[,2], type = "n", pch = 16, col = "blue", ylim = c(-1,12), xlim = c(-1,12), xlab = "", xaxt = "n", ylab = "", yaxt = "n")
 
-for(i in 1:29)
-  for(j in i:29)
-    segments(xl[i], yl[i], xl[j], yl[j], lwd = C2[i,j]*4)
+for(i in 1:N.f)
+  for(j in i:N.f)
+    segments(xy.f[i,1], xy.f[i,2], xy.f[j,1], xy.f[j,2], lwd = CC[i,j]*4)
 
 plot(xl, yl, type = "n", pch = 16, col = "blue", ylim = c(-1,12), xlim = c(-1,12), xlab = "", xaxt = "n", ylab = "", yaxt = "n")
 
