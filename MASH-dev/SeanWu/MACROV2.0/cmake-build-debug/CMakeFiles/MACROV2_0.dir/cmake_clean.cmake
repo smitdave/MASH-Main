4ea5c5f4d4678c177f1abd94eb3d1a551a3497e8
slwu89 <slwu89@berkeley.edu>
2017-11-21 12:21:37 -0800

    stash

---
+++ MASH-dev/SeanWu/MACROV2.0/cmake-build-debug/CMakeFiles/MACROV2_0.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MACROV2_0.dir/main.cpp.o"
+  "../build/MACROV2_0.pdb"
+  "../build/MACROV2_0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MACROV2_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
