fe83a55b289698bd117265e3d0bc79bb3f6624a0
smitdave <smitdave@gmail.com>
2017-11-22 11:52:39 -0800

    demo, V1.3.1

--- MASH-dev/DavidSmith/MBITES-Demo/PeriDomesticSet.R
+++ MASH-dev/DavidSmith/MBITES-Demo/PeriDomesticSet.R
@@ -5,27 +5,27 @@ xy.f = clusters2xy(fclusters)
 pointsets = list()
 pdf("overlap.pdf")
 xy.l = clusters2xy(lclusters)
-plot.fl(xy.f, xy.l)
+pointSetDiagnostic(xy.f, xy.l)
 pointsets[[1]] = list(xy.f =xy.f, xy.l=xy.l)
 
 newlcluster = c(fclusters[1], lclusters[2:25])
 xy.l = clusters2xy(newlcluster)
-plot.fl(xy.f, xy.l)
+pointSetDiagnostic(xy.f, xy.l)
 pointsets[[2]] = list(xy.f =xy.f, xy.l=xy.l)
 
 for(i in 2:24){
   newlcluster = c(fclusters[1:i], lclusters[(i+1):25])
   xy.l = clusters2xy(newlcluster)
-  plot.fl(xy.f, xy.l)
+  pointSetDiagnostic(xy.f, xy.l)
   pointsets[[i+1]] = list(xy.f =xy.f, xy.l=xy.l)
 }
 
 xy.l = clusters2xy(fclusters)
-plot.fl(xy.f, xy.l)
+pointSetDiagnostic(xy.f, xy.l)
 pointsets[[26]] = list(xy.f =xy.f, xy.l=xy.l)
 
 dev.off(dev.cur()) 
 
-xy.l = clusters2xy(lclusters)
+#xy.l = clusters2xy(lclusters)
 
-pointSetDiagnostic(xy.f, xy.l)
+#pointSetDiagnostic(xy.f, xy.l)
