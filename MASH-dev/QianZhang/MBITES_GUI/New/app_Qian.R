3ccfc43fe21ab00bdbcc7df55bc05f373b1dd59c
Qian Zhang <zqstat@gmail.com>
2017-11-29 15:02:03 -0800

    Debugged 'save into json'

--- MASH-dev/QianZhang/MBITES_GUI/New/app_Qian.R
+++ MASH-dev/QianZhang/MBITES_GUI/New/app_Qian.R
@@ -1124,56 +1124,108 @@ mbitesGadget = function(...){
     
     observeEvent(input$save_inputs_bout, {
       # Define inputs to save
-      f_param_name <- c('F_time_m', 'F_time_h', 'F_succeed', 'F_surv',
-        'F_wts')
-      b_param_name <- c('B_time', 'B_succeed', 'B_surv', 'B_wts',
+      f_param_name <- c('F_time_m', 'F_time_h', 'F_succeed', 'F_surv')
+      b_param_name <- c('B_time_m', 'B_time_h','B_succeed', 'B_surv',
         'surviveH', 'probeH', 'surviveprobeH', 'feedH',
         'surviveZ', 'feedZ', 'bm_a', 'bm_b', 'overfeed', 'of_a', 'of_b', 'preGblood',
         'Q')
-      r_param_name <- c('R_time', 'R_surv', 'R_wts', 'REFEED', 'rf_a', 'rf_b')
-      l_param_name <- c('L_time', 'L_succeed','L_surv', 'L_wts')
-      o_param_name <- c('O_time', 'O_succeed', 'O_surv','O_wts')
-      m_param_name <- c('M_time', 'M_succeed', 'M_surv', 'M_wts')
-      s_param_name <- c('S_time', 'S_succeed', 'S_surv', 'S_wts', 'preGsugar')
+      r_param_name <- c('R_time_m', 'R_time_h','R_surv', 'REFEED', 'rf_a', 'rf_b')
+      l_param_name <- c('L_time_m', 'L_time_h','L_succeed','L_surv')
+      o_param_name <- c('O_time_m', 'O_time_h','O_succeed', 'O_surv')
+      m_param_name <- c('M_time_m', 'M_time_h','M_succeed', 'M_surv')
+      s_param_name <- c('S_time_m', 'S_time_h','S_succeed', 'S_surv', 'preGsugar')
       # Declare inputs
       inputs_bout <- NULL
       inputs_name <- NULL
       # Append all inputs before saving to folder
       if(input$showF){
-        inputs_name <- append(inputs_name,f_param_name)
+        
         for(input.i in f_param_name){
+          if(length(input[[input.i]]) != 0){
+          inputs_name <- append(inputs_name,input.i)
           inputs_bout <- append(inputs_bout, input[[input.i]])
-      }}
+      }}}
+
       if(input$showB){
-        inputs_name <- append(inputs_name,b_param_name)
         for(input.i in b_param_name){
+          if(length(input[[input.i]]) != 0){
+          inputs_name <- append(inputs_name,input.i)
           inputs_bout <- append(inputs_bout, input[[input.i]])
-      }}
+      }}}
+
       if(input$showR){
-        inputs_name <- append(inputs_name,r_param_name)
         for(input.i in r_param_name){
+          if(length(input[[input.i]]) != 0){
+          inputs_name <- append(inputs_name,input.i)
           inputs_bout <- append(inputs_bout, input[[input.i]])
-      }}
+      }}}
+
       if(input$showL){
-        inputs_name <- append(inputs_name,l_param_name)
         for(input.i in l_param_name){
+          if(length(input[[input.i]]) != 0){
+          inputs_name <- append(inputs_name,input.i)
           inputs_bout <- append(inputs_bout, input[[input.i]])
-      }}
+      }}}
+
       if(input$showO){
-        inputs_name <- append(inputs_name,o_param_name)
         for(input.i in o_param_name){
+          if(length(input[[input.i]]) != 0){
+          inputs_name <- append(inputs_name,input.i)
           inputs_bout <- append(inputs_bout, input[[input.i]])
-      }}
+      }}}
+
       if(input$showM){
-        inputs_name <- append(inputs_name,m_param_name)
         for(input.i in m_param_name){
+          if(length(input[[input.i]]) != 0){
+          inputs_name <- append(inputs_name,input.i)
           inputs_bout <- append(inputs_bout, input[[input.i]])
-      }}
+      }}}
+
       if(input$showS){
-        inputs_name <- append(inputs_name,s_param_name)
-        for(input.i in s_param_name){
+        for(input.i in m_param_name){
+          if(length(input[[input.i]]) != 0){
+          inputs_name <- append(inputs_name,input.i)
           inputs_bout <- append(inputs_bout, input[[input.i]])
-      }}
+      }}}
+
+
+
+
+      # if(input$showF){
+      #   inputs_name <- append(inputs_name,f_param_name)
+      #   for(input.i in f_param_name){
+      #     inputs_bout <- append(inputs_bout, input[[input.i]])
+      # }}
+      # if(input$showB){
+      #   inputs_name <- append(inputs_name,b_param_name)
+      #   for(input.i in b_param_name){
+      #     inputs_bout <- append(inputs_bout, input[[input.i]])
+      # }}
+      # if(input$showR){
+      #   inputs_name <- append(inputs_name,r_param_name)
+      #   for(input.i in r_param_name){
+      #     inputs_bout <- append(inputs_bout, input[[input.i]])
+      # }}
+      # if(input$showL){
+      #   inputs_name <- append(inputs_name,l_param_name)
+      #   for(input.i in l_param_name){
+      #     inputs_bout <- append(inputs_bout, input[[input.i]])
+      # }}
+      # if(input$showO){
+      #   inputs_name <- append(inputs_name,o_param_name)
+      #   for(input.i in o_param_name){
+      #     inputs_bout <- append(inputs_bout, input[[input.i]])
+      # }}
+      # if(input$showM){
+      #   inputs_name <- append(inputs_name,m_param_name)
+      #   for(input.i in m_param_name){
+      #     inputs_bout <- append(inputs_bout, input[[input.i]])
+      # }}
+      # if(input$showS){
+      #   inputs_name <- append(inputs_name,s_param_name)
+      #   for(input.i in s_param_name){
+      #     inputs_bout <- append(inputs_bout, input[[input.i]])
+      # }}
 
       # Inputs data.frame
       inputs_data_frame <- data.frame(inputId = inputs_name, value = inputs_bout)
