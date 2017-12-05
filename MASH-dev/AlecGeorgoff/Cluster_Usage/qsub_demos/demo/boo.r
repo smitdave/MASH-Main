ec25d00eac1e1630c6d0fb0baf7ce1820ee71c59
Alec Georgoff <georgoff@cn495.ihme.washington.edu>
2017-12-05 13:36:37 -0800

    added qsub output test files, created AlecGeorgoff directory

---
+++ MASH-dev/AlecGeorgoff/Cluster_Usage/qsub_demos/demo/boo.r
@@ -0,0 +1,24 @@
+# Use this script to start a job with foo.r using a qsub
+seed <- 1
+
+jname <- paste0("mash-macro-qsub_", seed)
+slots <- 1
+mem <- slots*2
+holds <- "no_holds"
+
+# Setting up the qsub job
+sys.sub <- paste0("qsub -cwd -N ", jname, " -pe multi_slot ", slots, " -l mem_free=", mem, "G -hold_jid ", holds,
+                  " -o foo_outputs/boo_output", # where output will go
+                  " -e foo_outputs/boo_errors"  # where errors will go
+                  )
+args <- paste(seed, sep=" ")
+# This is a shell script, says which R to use, and passes arguments to the R script
+shell <- "r_shell.sh" # Add this to a r_shell.sh file #$ -S /bin/sh # r_dock <$1 --no-save $@
+# R script to execute
+script <- "foo.r"
+
+# Print to screen the qsub
+print(paste(sys.sub, shell, script, "\"", args, "\""))
+
+# Execute!
+system(paste(sys.sub, shell, script, "\"", args, "\""))
---
+++ MASH-dev/AlecGeorgoff/Cluster_Usage/qsub_demos/demo/r_shell.sh
@@ -0,0 +1,5 @@
+#$ -S /bin/sh
+#/usr/local/R-current/bin/R <$1 --no-save $2 $3 $4 $5
+/usr/local/R-current/bin/R <$1 --no-save $@
+
+#r_dock <$1 --no-save $@
