c4c7dd54565178552634a1beb2eed9294f2337a6
Alec Georgoff <georgoff@ihme.washington.edu>
2017-12-14 14:58:44 -0800

    added script to loop over multiple qsubs

--- MASH-dev/AlecGeorgoff/Benchmarking/qsub_TEST_multi.R
+++ MASH-dev/AlecGeorgoff/Benchmarking/qsub_TEST_multi.R
@@ -1,18 +1,19 @@
-n_params <- c(1,5,10)
+n_humans <- c(300000)
 
-for (n in n_params) {
+for (n in n_humans) {
   
   jname <- paste0("Test-Macro_qsub_script_test_", n)
-  slots <- 1
+  slots <- 16
   mem <- slots*2
   holds <- "no_holds"
+  output_folder <- paste0("n_humans_equals_", n, "_run2")
   
   sys.sub <- paste0("qsub -cwd -N ", jname, " -pe multi_slot ", slots, " -l mem_free=", mem, "G -hold_jid ", holds)
-  args <- paste0("--args ", n)
+  args <- paste("--args", n, output_folder)
   # This is a shell script, says which R to use, and passes arguments to the R script
   shell <- "mash_shell_script.sh"
   # R script to execute
-  script <- "TEST-MACRO.R"
+  script <- "TEST-MACRO-BENCHMARK.R"
   
   # Print to screen the qsub
   print(paste(sys.sub, shell, script, args))
