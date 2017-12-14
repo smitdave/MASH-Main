eb3f7ab92a5d3a41ad849d6bca5903eade3157d7
Hector M. Sanchez C <chipdelmal@gmail.com>
2017-12-14 15:46:19 -0600

    Creating a random graph and exporting edges list + centrality

--- MASH-dev/HectorSanchez/NetworksScripts/networksTest.py
+++ MASH-dev/HectorSanchez/NetworksScripts/networksTest.py
@@ -1,5 +1,12 @@
 import networkx as nx
+import csv
 
-G=nx.complete_graph(100)
-deg=nx.degree_centrality(G)
-print(deg)
+# Generate a random network
+G=nx.watts_strogatz_graph(100,3,.2)
+# Calculate centrality
+deg=nx.betweenness_centrality(G)
+# Export adjacency list
+nx.write_adjlist(G,"OUT_networkExportTest.adjlist")
+# Export centralities list
+with open('OUT_centralities.csv', 'w') as f:
+    [f.write('{0},{1}\n'.format(key, value)) for key, value in deg.items()]
