e718e0d6de0260102117370ee934fee135323dc8
slwu89 <slwu89@berkeley.edu>
2018-01-05 08:11:30 -0800

    reupload old cpp src

---
+++ MASH-dev/SeanWu/MACRO-CPP/MACRO-CPP/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  MACRO-CPP
+//
+//  Created by Sean Wu on 11/9/17.
+//  Copyright © 2017 Sean Wu. All rights reserved.
+//
+
+#include <iostream>
+
+#include "test.hpp"
+
+int main(int argc, const char * argv[]) {
+    
+//    testHuman();
+    testImmune();
+    
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
