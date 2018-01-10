7895d7df7768f7ccc77c2e580eb8c535b1f881a1
John Henry <henry529@ihme.washington.edu>
2018-01-10 14:48:44 -0800

    optimized tteDiurnal rejection

--- MASH-dev/JohnHenry/MBITES_Files/tteDiurnal.R
+++ MASH-dev/JohnHenry/MBITES_Files/tteDiurnal.R
@@ -1,3 +1,7 @@
+
+################# First Method - CDF Inversion Using Newton's Method ###################
+#################         More Accurate, Less Efficient              ###################
+
 par(mfrow=c(2,2))
 
 ff = function(t, peak=0,amp=1){
@@ -49,26 +53,53 @@ count = c(0,hist(v,freq=F,breaks=100)$count)
 plot(seq(0,7,7/(length(count)-1)),cumsum(count)/sum(count),type="l",ylab="empirical tteCDF",ylim=c(0,1))
 
 
-##################### second method - rejection method #####################
-#####################         more efficient           ##################### 
+##################### Second Method - Rejection Method #####################
+#####################   Less Accurate, More Efficient  ##################### 
 
 par(mfrow=c(1,1))
 
+## need to define pdf f1, dominating pdf f2, and constant ST c*f2 > f1
+## the smaller the constant, the more efficient the method
+
+## f1
 gg = function(t,lam){
   lam*(1+cos(2*pi*t))*exp(-lam*(t+sin(2*pi*t)/(2*pi)))
 }
 
+## f2
 GG = function(t,lam){
   lam*exp(-lam*t)
 }
 
+## to find minimum c to optimize efficiency, need to solve
+## f1 <= c*f2 for minimal value of c; in this case,
+## this becomes the maximum value of the function
+## 1+cos(2*pi*t)-2*pi*sin(2*pi*t)-sin^2(2*pi*t)
+
+g = function(t,lam){
+  (1+cos(2*pi*t))*exp(-lam*sin(2*pi*t)/(2*pi))
+}
+
+gp = function(t){
+ cos(2*pi*t)+1+2*pi*sin(2*pi*t)-sin(2*pi*t)^2
+}
+
+gpp = function(t){
+  2*pi*(-sin(2*pi*t)+2*pi*cos(2*pi*t)+2*sin(2*pi*t)*cos(2*pi*t))
+}
+
+xval = newton(gp,gpp,2,10^-4)
+lam = 1
+c = g(xval,lam)
+
+## plotting to show dominance with constant c = 2.0495
 plot(t,gg(t,1),type="l")
-lines(t,2.3*GG(t,1))
+lines(t,c*GG(t,1))
 
 tteDiurnal = function(N,lam){
   v = rep(0,N)
   for(i in 1:N){
-    c = 2.3
+    c = 2.0495
     vtemp = 0
     tt = 2
     u = 1
