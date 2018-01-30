f1d426109b40c92c4ff10203cef2df99287505ce
chipdelmal <chipdelmal@gmail.com>
2018-01-30 13:26:17 -0800

    Updating my Vector Control script for chord-diagram-based debugging
    This should have gone up to my VC branch but I forgot and now it's too late. I apologize for my mistake and may the gods forgive me.

--- MASH-dev/HectorSanchez/AssortedScripts/main.R
+++ MASH-dev/HectorSanchez/AssortedScripts/main.R
@@ -14,6 +14,9 @@
 rm(list=ls());gc()
 library(MASHmicro)
 library(R6)
+library(jsonlite)
+library(markovchain)
+library(circlize)
 # set.seed(42L)
 
 #DEBUG.MASHMICRO()
@@ -25,10 +28,10 @@ if(system("whoami",intern=TRUE)=="slwu89"){
   DIR="/Users/slwu89/Desktop/MASHOUT/"
 }else if(system("whoami",intern=TRUE)=="sanchez.hmsc"){
   #DIR="/Users/sanchez.hmsc/Desktop/MASHOUT/"
-  DIR="/Users/sanchez.hmsc/Documents/Github/MASH-Main/MASH-dev/HectorSanchez/VectorControl/Debug/"
-  source("/Users/sanchez.hmsc/Documents/Github/MASH-Main/MASH-dev/HectorSanchez/AssortedScripts/sourceEntireFolder.R")
-  sourceEntireFolder("/Users/sanchez.hmsc/Documents/Github/MASH-Main/MASH-MICRO/R/")
-  sourceEntireFolder("/Users/sanchez.hmsc/Documents/Github/MASH-Main/MASH-dev/HectorSanchez/VectorControl/")
+  DIR="/Users/sanchez.hmsc/Documents/Github/MASH-Master/MASH-dev/HectorSanchez/VectorControl/Debug/"
+  source("/Users/sanchez.hmsc/Documents/Github/MASH-Master/MASH-dev/HectorSanchez/AssortedScripts/sourceEntireFolder.R")
+  sourceEntireFolder("/Users/sanchez.hmsc/Documents/Github/MASH-Master/MASH-MICRO/R/")
+  sourceEntireFolder("/Users/sanchez.hmsc/Documents/Github/MASH-Master/MASH-dev/HectorSanchez/VectorControl/")
 }else if(system("whoami",intern=TRUE)=="chipdelmal"){
   DIR="/Users/chipdelmal/Desktop/MASHOUT/"
   source("/Users/chipdelmal/Documents/Github/MASH-Main/MASH-dev/HectorSanchez/AssortedScripts/sourceEntireFolder.R")
@@ -36,62 +39,111 @@ if(system("whoami",intern=TRUE)=="slwu89"){
   sourceEntireFolder("/Users/chipdelmal/Documents/Github/MASH-Main/MASH-dev/HectorSanchez/VectorControl/")
 }
 
-#library(MASHmicro)
-#library(MASHmacro)
+###############################################################################
+# Chord diagram
+###############################################################################
+# oneHistory: a single mosquito's JSON outfile
+transitionsInMosquitoStates <- function(oneHistory, stateSpace = c("D","M","F","B","R","L","O","S","E")){
+  states = oneHistory$stateH
+  createSequenceMatrix(stringchar = unlist(states[-1]),possibleStates = stateSpace)
+}
+
+transitionsInMosquitoPopulation <- function(popHistory, stateSpace = c("D","M","F","B","R","L","O","S","E")){
+  transMatrices = lapply(X = popHistory,FUN = transitionsInMosquitoStates)
+  transitions = Reduce(f = "+",x = transMatrices)
+  transitions[stateSpace,stateSpace]
+}
+
+circlizeStatesTransitionMatrix <- function(history, stateSpace = c("D","M","F","B","R","L","O","S","E")){
+  transitions=transitionsInMosquitoPopulation(history,stateSpace=stateSpace)
+  colors=c("#555555","#95E455","pink","red","purple","cyan","blue","yellow","grey")
+  chordDiagramFromMatrix(transitions,directional=1,grid.col=colors,direction.type="arrows",self.link=2)
+}
+###############################################################################
+
+###############################################################################
 # setup
 Humans.MICRO.Setup()
-PfSI.MICRO.Setup(Pf_c = 1,Pf_b = 1,LatentPf = 1,DurationPf = 20)
+PfSI.MICRO.Setup()
 AQUA.Emerge.Setup()
 
 # MBITES setup
-MBITES.Generic.Setup()
-MBITES.Complex.Setup(aquaModule = "emerge",timing = "exponential")
-MBITES.Male.Setup(timing = "exponential")
+MBITES.Setup(aquaModule = "emerge",timing = "exponential")
 
 # SEARCH setup
-SEARCH.Kernel.Setup(MBITES = "FULL")
+MBITES.Search.Setup(module = "kernel")
 
 # landscape parameters
 nAqua = 20
+nPeriDom = 0
 nFeed = 15
 nSugar = 12
 nMate = 10
 emerge_par = list(N = nAqua,lambda = 25, lambdaWeight = NULL, offset = NULL)
 landscape_par = Landscape.Parameters(nFeed = nFeed,nAqua = nAqua,nMate = nMate,nSugar = nSugar,pointGen = "lattice",module = "emerge",modulePars = emerge_par)
 
+# set up the peri-domestic bit
+periDomestic = rep(FALSE,nFeed)
+lambda = replicate(n = nFeed,expr = NULL,simplify = FALSE)
+module = rep(NA,nFeed)
+
+if(nPeriDom>0){
+  periDomestic[1:nPeriDom] = TRUE
+  lambda[1:nPeriDom] = simpleLambda_Emerge(N = nPeriDom,lambda = 5)
+  module[1:nPeriDom] = "emerge"
+}
+
+landscape_par$FeedingSite_PAR$periDomestic = periDomestic
+landscape_par$FeedingSite_PAR$lambda = lambda
+landscape_par$FeedingSite_PAR$module = module
+
 # human parameters
-human_par = MASHmacro::HumanPop.Parameters(nSite = nFeed,siteSize = 10,siteMin = 2)
+patch_humans = rep(1,nFeed)
+n_humans = sum(patch_humans)
+patch_id = rep(x = 1:nFeed,patch_humans)
+home_id = rep(x = 1:nFeed,patch_humans)
+human_ages = unlist(lapply(X = patch_humans,FUN = MASHmacro:::siteAges_HumanPop))
+human_bWeight = MASHmacro:::bitingWeight_HumanPop(human_ages)
+human_par = lapply(X = 1:n_humans,function(i){
+  list(
+    houseID = home_id[i],
+    patchID = patch_id[i],
+    homeHouseID = home_id[i],
+    homePatchID = patch_id[i],
+    age = human_ages[i],
+    bWeight = human_bWeight[i]
+
+  )
+})
 
 # M-BITES parameters
 nMosy = 50
-mbites_par_female = MBITES.Complex.Parameters(PfEIP = 1 )
-mbites_par_male = MBITES.Male.Parameters(maleHistory = TRUE)
+mbites_par_female = MBITES.Complex.Parameters(PfEIP = 1,SUGAR = TRUE,MATE = TRUE)
 mosquito_par = list(
-  N_female = nMosy,
-  N_male = nMosy,
-  ix_female = rep(1,nMosy),
-  ix_male = rep(1,nMosy),
-  genotype_female = rep(1,nMosy),
-  genotype_male = rep(1,nMosy),
-  MBITES_PAR_FEMALE = mbites_par_female,
-  MBITES_PAR_MALE = mbites_par_male
+N_female = nMosy,
+ix_female = rep(1,nMosy),
+genotype_female = rep(1,nMosy),
+MBITES_PAR_FEMALE = mbites_par_female
 )
 
 MicroTile = Tile$new(Landscape_PAR = landscape_par,HumanPop_PAR = human_par,MosquitoPop_PAR = mosquito_par,directory = DIR)
 MicroTile$get_HumanPop()$init_ActivitySpace()
-MicroTile$get_HumanPop()$init_PfSI(PfPR = 0.95)
+
+# PfPR
+pfpr = rep(0.5,nFeed)
+MicroTile$simMICRO_oneRun(tMax = 50,PfPAR = pfpr,verbose = TRUE,trackPop = TRUE)
 
 ###### Distribute ######################################################################
 ### Sugar ######################################################
 sugarSitesNumber=MicroTile$get_Landscape()$get_SugarSitesN()
 for(i in 1:sugarSitesNumber){
-  atsbTest=ATSB$new(id=i,killProbability=0,repelProbability=0)
+  atsbTest=ATSB$new(id=i,killProbability=1,repelProbability=0)
   MicroTile$get_Landscape()$get_SugarSites(i)$set_attractiveSugarBait(atsbTest)
 }
 ### Aquatic ####################################################
 aquaSitesNumber=MicroTile$get_Landscape()$get_AquaSitesN()
 for(i in 1:aquaSitesNumber){
-  ovitrapTest=Ovitrap$new(id=i,killProbability=0,repelProbability=0)
+  ovitrapTest=Ovitrap$new(id=i,killProbability=1,repelProbability=0)
   MicroTile$get_Landscape()$get_AquaSites(i)$set_ovitrap(ovitrapTest)
 }
 ### Feeding ####################################################
@@ -107,11 +159,20 @@ for(i in 1:matingSitesNumber){
   MicroTile$get_Landscape()$get_MatingSites(i)$set_swarmSpray(swarmSprayTest)
 }
 ###### Run #############################################################################
-MicroTile$simMICRO_oneRun(tMax = 750,verbose = TRUE,trackPop = TRUE)
+MicroTile$simMICRO_oneRun(tMax = 500,PfPAR = pfpr,verbose = TRUE,trackPop = TRUE)
 
 ########################################################################################
 ###### TESTS ###########################################################################
 ########################################################################################
+# files from each simulation
+files_l = system(command = paste0("ls ",DIR,"MOSQUITO/"),intern = TRUE)
+hist_l  = files_l[grep(pattern="History",x=files_l)] # individual json histories
+pop_l = files_l[grep(pattern = "Pop",x = files_l)] # population csv
+mHist   = fromJSON(txt=paste0(DIR,"MOSQUITO/",hist_l[[1]]),flatten = FALSE,simplifyVector=FALSE)
+nullIx = which(vapply(X = mHist,FUN = function(x){x$ID[[1]]},FUN.VALUE = character(1)) == "NULL")
+mHist = mHist[-nullIx]
+circlizeStatesTransitionMatrix(history = mHist)
+#mPop = read.table(file = paste0(DIR,"MOSQUITO/",pop_l),header = TRUE,sep = ",")
 
 ###### Get/Set #########################################################################
 # atsbTest=ATSB$new(id=10)
