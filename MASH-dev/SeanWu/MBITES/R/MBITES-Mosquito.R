c65071e70b69535163403c8f5cc46752fd6905e9
slwu89 <slwu89@berkeley.edu>
2018-02-14 18:24:49 -0800

    minor change

---
+++ MASH-dev/SeanWu/MBITES/R/MBITES-Mosquito.R
@@ -0,0 +1,62 @@
+###############################################################################
+#         __  ___      ____  _____________________
+#        /  |/  /     / __ )/  _/_  __/ ____/ ___/
+#       / /|_/ /_____/ __  |/ /  / / / __/  \__ \
+#      / /  / /_____/ /_/ // /  / / / /___ ___/ /
+#     /_/  /_/     /_____/___/ /_/ /_____//____/
+#
+#     MBITES-Mosquito
+#     MBITES Team
+#     February 2018
+#
+###############################################################################
+
+
+#' Mosquito Abstract Base Class
+#'
+#' All mosquitoes inherit from the \code{Mosquito} abstract base class object.
+#'
+#' @docType class
+#' @format An \code{\link{R6Class}} generator object
+#' @keywords R6 class
+#'
+#' @section **Constructor**:
+#'  * argument: im an agument!
+#'
+#' @section **Methods**:
+#'  * method: im a method!
+#'
+#' @section **Fields**:
+#'  * id: character of format tEmerge_id_genotype (ie; "5_42_2" is the 42nd mosquito emerging on day 5 with genotype 2)
+#'  * field: im a field!
+#'
+#' @export
+Mosquito <- R6::R6Class(classname = "Mosquito",
+                 portable = TRUE,
+                 cloneable = FALSE,
+                 lock_class = FALSE,
+                 lock_objects = FALSE,
+
+                 # public members
+                 public = list(
+
+                   ##############################################################
+                   # constructor
+                   ##############################################################
+
+                   initialize = function(){
+
+                   } # end constructor
+
+                 ),
+
+                 # private members
+                 private = list(
+
+                   id             = integer(1), # integer id
+                   
+
+                 )
+
+
+)
