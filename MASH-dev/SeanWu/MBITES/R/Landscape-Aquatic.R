6cf90035d8cfddf8facde2f75905ae776e0b5452
slwu89 <slwu89@berkeley.edu>
2018-02-19 13:15:21 -0800

    sketch of point-resource

---
+++ MASH-dev/SeanWu/MBITES/R/Landscape-Aquatic.R
@@ -0,0 +1,77 @@
+###############################################################################
+#         __  ___      ____  _____________________
+#        /  |/  /     / __ )/  _/_  __/ ____/ ___/
+#       / /|_/ /_____/ __  |/ /  / / / __/  \__ \
+#      / /  / /_____/ /_/ // /  / / / /___ ___/ /
+#     /_/  /_/     /_____/___/ /_/ /_____//____/
+#
+#     Landscape-Resource-Aquatic Habitat
+#     MBITES Team
+#     February 2018
+#
+###############################################################################
+
+
+#' Landscape Aquatic Habitat Resource Class
+#'
+#' A \code{Aqua_Resource} is a type of resource at a \code{\link[MBITES]{Site}} where mosquitoes travel for oviposition of eggs
+#' and from which new imagos (adult mosquitoes) emerge from. Dynamics of aquatic habitats are simulated by a reference (pimpl idiom)
+#' to the specific model chosen.
+#'
+#'
+#'
+#' @docType class
+#' @format An \code{\link{R6Class}} generator object
+#' @keywords R6 class
+#'
+#' @section **Constructor**:
+#'  * argument: im an agument!
+#'
+#' @section **Methods**:
+#'  * method: im a method!
+#'
+#' @section **Fields**:
+#'  * field: im a field!
+#'
+#' @export
+Aqua_Resource <- R6::R6Class(classname = "Aqua_Resource",
+                 portable = TRUE,
+                 cloneable = FALSE,
+                 lock_class = FALSE,
+                 lock_objects = FALSE,
+
+                 # public members
+                 public = list(
+
+                   # begin constructor
+                   initialize = function(){
+
+                   }, # end constructor
+
+                   # one day population dynamics
+                   one_day = function(){
+                     # add newly oviposited egg batches to aquatic population
+                     # private$AquaPopP$add_Egg(private$EggQ)
+
+                     # run one day simulation of aquatic population
+                     # private$AquaPopP$one_day()
+
+                     # add emerging adults to mosquito population
+                     # private$SiteP$get_TilePointer()$get_MosquitoPop()$add_Imago(private$ImagoQ)
+                   }
+
+                 ),
+
+                 # private members
+                 private = list(
+
+                   AquaPopP            = NULL, # reference (pimpl) to aquatic dynamic model
+                   EggQ                = list(), # list of egg batches
+                   ImagoQ              = list(), # list of newly emerging imagos
+                   SiteP               = NULL # pointer to my enclosing Site (has-a relationship; Sites manage Resource lifespans)
+
+
+                 )
+
+
+)
---
+++ MASH-dev/SeanWu/MBITES/R/Landscape-Feeding.R
@@ -0,0 +1,60 @@
+###############################################################################
+#         __  ___      ____  _____________________
+#        /  |/  /     / __ )/  _/_  __/ ____/ ___/
+#       / /|_/ /_____/ __  |/ /  / / / __/  \__ \
+#      / /  / /_____/ /_/ // /  / / / /___ ___/ /
+#     /_/  /_/     /_____/___/ /_/ /_____//____/
+#
+#     Landscape-Resource-Feeding
+#     MBITES Team
+#     February 2018
+#
+###############################################################################
+
+
+#' Landscape Feeding Resource Class
+#'
+#' A \code{Feeding_Resource} is a type of resource at a \code{\link[MBITES]{Site}} where mosquitoes can expect to find
+#' human or other vertebrate hosts when seeking a blood meal.
+#'
+#'
+#' @docType class
+#' @format An \code{\link{R6Class}} generator object
+#' @keywords R6 class
+#'
+#' @section **Constructor**:
+#'  * argument: im an agument!
+#'
+#' @section **Methods**:
+#'  * method: im a method!
+#'
+#' @section **Fields**:
+#'  * field: im a field!
+#'
+#' @export
+Feeding_Resource <- R6::R6Class(classname = "Feeding_Resource",
+                 portable = TRUE,
+                 cloneable = FALSE,
+                 lock_class = FALSE,
+                 lock_objects = FALSE,
+
+                 # public members
+                 public = list(
+
+                   # begin constructor
+                   initialize = function(){
+
+                   } # end constructor
+
+                 ),
+
+                 # private members
+                 private = list(
+
+                   # SiteP             = NULL, # pointer to my enclosing Site (has-a relationship; Sites manage Resource lifespans)
+
+
+                 )
+
+
+)
