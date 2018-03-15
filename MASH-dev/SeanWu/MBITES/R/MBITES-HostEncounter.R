5ea051a70c45e2277aabc509c4c8b6c11c682a67
slwu89 <slwu89@berkeley.edu>
2018-03-15 15:15:03 -0700

    write host encounter and oviposition routines

--- MASH-dev/SeanWu/MBITES/R/MBITES-HostEncounter.R
+++ MASH-dev/SeanWu/MBITES/R/MBITES-HostEncounter.R
@@ -1,1 +1,79 @@
-print("Host Encounter routines haven't been coded yet!")
+###############################################################################
+#       __  ___      ____  _____________________
+#      /  |/  /     / __ )/  _/_  __/ ____/ ___/
+#     / /|_/ /_____/ __  |/ /  / / / __/  \__ \
+#    / /  / /_____/ /_/ // /  / / / /___ ___/ /
+#   /_/  /_/     /_____/___/ /_/ /_____//____/
+#
+#   MBITES-Host Encounter
+#   MBITES Team
+#   March 2018
+#
+###############################################################################
+
+#' MBITES: Host Encounter
+#'
+#'
+#' Due to the different design of the R6/C++ OOP version of MASH-MICRO, all
+#' host probing and infection functions are moved to their respective PATHOGEN
+#' method files PATHOGEN-XX-Methods.R
+#'
+#' @name hostEncounter
+NULL
+#> NULL
+
+###############################################################################
+# Human Host Encounter
+###############################################################################
+
+#' MBITES-BRO: Human Host Encounter for \code{\link{MosquitoFemale}}
+#'
+#' After calling \code{\link{mbites_chooseHost}}, the mosquito encounters the human host and attempts to feed.
+#'  * This method is bound to \code{MosquitoFemale$humanEncounter()}.
+#'
+mbites_humanEncounter <- function(){
+  if(runif(1) < 1-MBITES:::Parameters$get_surviveH()){
+    # does not survive to probe
+    private$state = "D"
+  # survives to probe
+  } else {
+    if(runif(1) < MBITES:::Parameters$get_probeH()){
+      # undeterred, probes the host
+      self$probeHost() # PATHOGEN-XX.R (mosy -> host transmission)
+      if(runif(1) < MBITES:::Parameters$get_surviveProbeH()){
+        # does not survive to blood feed
+        private$state = "D"
+      # survives to blood feed
+      } else {
+        if(runif(1) < MBITES:::Parameters$get_feedH()){
+          # successfully begins blood feeding
+          self$BloodMeal() # MBITES-Energetics.R
+          self$feedHost() # PATHOGEN-XX.R
+        }
+      }
+    }
+  }
+}
+
+
+###############################################################################
+# Zoo Host Encounter
+###############################################################################
+
+#' MBITES: Non-Human Host Encounter for \code{\link{MosquitoFemale}}
+#'
+#' After calling \code{\link{mbites_chooseHost}}, the mosquito encounters the non-human host and attempts to feed.
+#'  * This method is bound to \code{MosquitoFemale$zooEncounter()}.
+#'
+mbites_zooEncounter <- function(){
+  if(runif(1) < 1-MBITES:::Parameters$get_surviveZ()){
+    # does not survive to feed
+    private$state = "D"
+  # survives to feed
+  } else {
+    if(runif(1) < MBITES:::Parameters$get_feedZ()){
+      # successfully begins blood feeding
+      self$BloodMeal() # MBITES-Energetics.R
+    }
+  }
+}
--- MASH-dev/SeanWu/MBITES/R/MBITES-Oviposition.R
+++ MASH-dev/SeanWu/MBITES/R/MBITES-Oviposition.R
@@ -1,1 +1,63 @@
-print("oviposition routines haven't been coded yet!")
+###############################################################################
+#       __  ___      ____  _____________________
+#      /  |/  /     / __ )/  _/_  __/ ____/ ___/
+#     / /|_/ /_____/ __  |/ /  / / / __/  \__ \
+#    / /  / /_____/ /_/ // /  / / / /___ ___/ /
+#   /_/  /_/     /_____/___/ /_/ /_____//____/
+#
+#   MBITES-Oviposition
+#   MBITES Team
+#   March 2018
+#
+###############################################################################
+
+#' MBITES: Oviposition
+#'
+#' write about me!
+#'
+#'
+#'
+#' @name Oviposition
+NULL
+#> NULL
+
+
+###############################################################################
+# Emerge
+###############################################################################
+
+#' M-BITES: Lay Eggs for 'Emerge' \code{\link{MosquitoFemale}}
+#'
+#' During an oviposition bout \code{\link{mbites_boutO}}, lay eggs (this is just a filler to clear out the \code{batch} field of the mosquito; egg laying is not implemented in any modules relying on "Emerge" Aquatic Ecology module)
+#'  * This method is bound to \code{MosquitoFemale$layEggs()}.
+#'
+mbites_layEggs_Emerge <- function(){
+  if(runif(1) < MBITES:::Parameters$get_O_succeed()){
+    private$batch = 0
+    private$stateNew = "F"
+    private$boutFail = 0L
+  } else {
+    private$boutFail = private$boutFail + 1L
+  }
+}
+
+
+###############################################################################
+# EL4P
+###############################################################################
+
+#' M-BITES: Lay Eggs for 'EL4P' \code{\link{MosquitoFemale}}
+#'
+#' During an oviposition bout \code{\link{mbites_boutO}}, lay eggs for 'EL4P' module of Aquatic Ecology.
+#'  * This method is bound to \code{MosquitoFemale$layEggs()}.
+#'
+mbites_layEggs_EL4P <- function(){
+  if(runif(1) < MBITES:::Parameters$get_O_succeed()){
+    private$aqua_res$EggQ$add2Q(private$batch,private$tNow)
+    private$batch = 0
+    private$stateNew = "F"
+    private$boutFail = 0L
+  } else {
+    private$boutFail = private$boutFail + 1L
+  }
+}
