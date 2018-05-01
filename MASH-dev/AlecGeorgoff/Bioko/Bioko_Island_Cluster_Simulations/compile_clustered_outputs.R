2c86554c93068ef7495b0e0d559b06ef3160e0ea
Alec Georgoff <AGeorgoff@Alecs-MacBook-Air.local>
2018-05-01 16:24:44 -0700

    more ensemble stuff

--- MASH-dev/AlecGeorgoff/Bioko/Bioko_Island_Cluster_Simulations/compile_clustered_outputs.R
+++ MASH-dev/AlecGeorgoff/Bioko/Bioko_Island_Cluster_Simulations/compile_clustered_outputs.R
@@ -50,8 +50,8 @@ files_ensemble <- files[grepl("Ensemble", files)]
 
 compiled_conversion_curves_single <- data.table(time = NA, location = NA, status = NA, N = NA)
 compiled_full_curves_single <- data.table(time = NA, location = NA, status = NA, N = NA)
-compiled_conversion_curves_ensemble <- data.table(time = NA, location = NA, status = NA, N = NA)
-compiled_full_curves_ensemble <- data.table(time = NA, location = NA, status = NA, N = NA)
+compiled_conversion_curves_ensemble <- data.table(time = NA, location = NA, status = NA, N = NA, ens_num = NA)
+compiled_full_curves_ensemble <- data.table(time = NA, location = NA, status = NA, N = NA, ens_num = NA)
 
 #######################
 ## Loop through files and correct location names (Single trajectory)
@@ -60,7 +60,7 @@ compiled_full_curves_ensemble <- data.table(time = NA, location = NA, status = N
 if (compile_single_trajectories) {
   for (i in clusters_to_compile) {
     # isolate the output folder for the current cluster:
-    file <- files[grepl(paste0("y_", i, "$"), files) & grepl("Single", files)]
+    file <- files_single[grepl(paste0("y_", i, "$"), files_single)]
     print(file)
     human.pathogen.path <- paste0(file, "/HumanPathogen_Run0.csv")
     human.move.path <- paste0(file, "/HumanMove_Run0.csv")
@@ -116,58 +116,65 @@ if (compile_single_trajectories) {
 ## Loop through files and correct location names (Ensemble trajectory)
 #######################
 
-if (compile_single_trajectories) {
+if (compile_ensemble_trajectories) {
   for (i in clusters_to_compile) {
     # isolate the output folder for the current cluster:
-    file <- files[grepl(paste0("y_", i, "$"), files) & grepl("Single", files)]
+    file <- files_ensemble[grepl(paste0("s_", i, "$"), files_ensemble)]
     print(file)
-    human.pathogen.path <- paste0(file, "/HumanPathogen_Run0.csv")
-    human.move.path <- paste0(file, "/HumanMove_Run0.csv")
+    for (j in ensemble_numbers) {
+      human.pathogen.path <- paste0(file, "/HumanPathogen_Run", j , ".csv")
+      human.move.path <- paste0(file, "/HumanMove_Run", j, ".csv")
+      print(human.pathogen.path)
 
-    # retrieve the number of humans in the clusters of interest:
-    patch_humans <- cluster.human.pops[c(i, c(42:48))]
+      # retrieve the number of humans in the clusters of interest:
+      patch_humans <- cluster.human.pops[c(i, c(42:48))]
 
-    # create conversion curve:
-    conversion_curve <- SIP.Conversion.Curves(human.pathogen.path = human.pathogen.path,
-                                              human.move.path = human.move.path,
-                                              patch_humans = patch_humans,
-                                              tMax = tMax)
+      # create conversion curve:
+      conversion_curve <- SIP.Conversion.Curves(human.pathogen.path = human.pathogen.path,
+                                                human.move.path = human.move.path,
+                                                patch_humans = patch_humans,
+                                                tMax = tMax)
 
-    # create full SIP curve:
-    full_curve <- SIP.FULL(t = conversion_curve,
-                           n = num_regions,
-                           tMax = tMax,
-                           status.list = c("S", "I", "P"))
+      # create full SIP curve:
+      full_curve <- SIP.FULL(t = conversion_curve,
+                             n = num_regions,
+                             tMax = tMax,
+                             status.list = c("S", "I", "P"))
 
-    # rename regions to their actual names:
-    conversion_curve$location <- as.character(conversion_curve$location)
-    conversion_curve[location == "2", location := "Off"]
-    conversion_curve[location == "3", location := "Baney"]
-    conversion_curve[location == "4", location := "Luba"]
-    conversion_curve[location == "5", location := "Malabo"]
-    conversion_curve[location == "6", location := "Moka"]
-    conversion_curve[location == "7", location := "Riaba"]
-    conversion_curve[location == "8", location := "Ureka"]
+      # add ensemble number to results:
+      conversion_curve$ens_num <- j
+      full_curve$ens_num <- j
 
-    # rename "location 1" to current cluster:
-    conversion_curve[location == "1", location := as.character(i)]
+      # rename regions to their actual names:
+      conversion_curve$location <- as.character(conversion_curve$location)
+      conversion_curve[location == "2", location := "Off"]
+      conversion_curve[location == "3", location := "Baney"]
+      conversion_curve[location == "4", location := "Luba"]
+      conversion_curve[location == "5", location := "Malabo"]
+      conversion_curve[location == "6", location := "Moka"]
+      conversion_curve[location == "7", location := "Riaba"]
+      conversion_curve[location == "8", location := "Ureka"]
 
-    # rename regions to their actual names:
-    full_curve$location <- as.character(full_curve$location)
-    full_curve[location == "2", location := "Off"]
-    full_curve[location == "3", location := "Baney"]
-    full_curve[location == "4", location := "Luba"]
-    full_curve[location == "5", location := "Malabo"]
-    full_curve[location == "6", location := "Moka"]
-    full_curve[location == "7", location := "Riaba"]
-    full_curve[location == "8", location := "Ureka"]
+      # rename "location 1" to current cluster:
+      conversion_curve[location == "1", location := as.character(i)]
 
-    # rename "location 1" to current cluster:
-    full_curve[location == "1", location := as.character(i)]
+      # rename regions to their actual names:
+      full_curve$location <- as.character(full_curve$location)
+      full_curve[location == "2", location := "Off"]
+      full_curve[location == "3", location := "Baney"]
+      full_curve[location == "4", location := "Luba"]
+      full_curve[location == "5", location := "Malabo"]
+      full_curve[location == "6", location := "Moka"]
+      full_curve[location == "7", location := "Riaba"]
+      full_curve[location == "8", location := "Ureka"]
 
-    # append to data sets:
-    compiled_conversion_curves_single <- rbind(compiled_conversion_curves_single, conversion_curve)
-    compiled_full_curves_single <- rbind(compiled_full_curves_single, full_curve)
+      # rename "location 1" to current cluster:
+      full_curve[location == "1", location := as.character(i)]
+
+      # append to data sets:
+      compiled_conversion_curves_ensemble <- rbind(compiled_conversion_curves_ensemble, conversion_curve)
+      compiled_full_curves_ensemble <- rbind(compiled_full_curves_ensemble, full_curve)
+    }
   }
 }
 
