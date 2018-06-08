145f8c6c07deb7e40d44c25563d469d3b96cddf3
Daniel Citron <dtcitron@uw.edu>
2018-06-08 10:58:10 -0700

    Added MBITES Vector Bionomics Calculator to my dev folder

---
+++ MASH-dev/DanielCitron/MBITES_vector_bionomics/README.md
@@ -0,0 +1,5 @@
+# Example usage of vector bionomics calculation scripts for MBITES
+
+#### - Daniel Citron, 6/8/2018
+
+<dl><hr></dl>
---
+++ MASH-dev/DanielCitron/MBITES_vector_bionomics/Using_Vector_Bionomics_Calculator.R
@@ -0,0 +1,31 @@
+require(jsonlite)
+require(data.table)
+## Change this to the right path
+source("/Users/dtcitron/Documents/MASH/MASH-Main/MASH-dev/DanielCitron/MBITES_vector_bionomics/Vector_Bionomics_Calculator.R")
+
+## Change this to the right path
+## This is just an example of MBITES output - you'll want to apply these scripts to other outputs later
+filename = "/Users/dtcitron/Documents/MASH/MASH-Main/MASH-dev/DanielCitron/MBITES_vector_bionomics/mosquito_F_1.json"
+
+## Read in the json file and convert it to a data.table
+## NB: the EIP I've specified here is made up nonsense, and should be replaced with a correct value later (ask Dave)
+bionomics.data <- combine.bionomics.data(filename, EIP=2)
+
+## These are the four sets of vector binomics related to transmission
+
+## Histogram of lifetimes
+lifetimes.distn <- lifetimes.distribution.data(bionomics.data)
+hist(lifetimes.distn, breaks = c(0:ceiling(max(lifetimes.distn)))-.5)
+
+## Histogram of feeding intervals
+feedtimes.distn <- feed.intervals.distribution.data(bionomics.data)
+hist(feedtimes.distn, breaks = c(0:ceiling(max(feedtimes.distn)))-.5)
+
+## Histogram of number of blood feeding per mosquito lifetime
+numbites.distn <- biting.distribution.data(bionomics.data)
+hist(numbites.distn, breaks = c(-1:ceiling(max(numbites.distn)))+.5)
+
+## Histogram of vectorial capacity
+# This is the one that's a little tricky - 
+VC.distn <- vc.distribution.data(bionomics.data)
+hist(VC.distn, breaks = c(0:(ceiling(max(VC.distn))+1))-.5)
