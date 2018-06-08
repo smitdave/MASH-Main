4c0fe414603bf36ef9b843383954b4d485e030e8
Daniel Citron <dtcitron@uw.edu>
2018-06-08 12:40:57 -0700

    updated vector bionomics calculator

--- MASH-dev/DanielCitron/MBITES_vector_bionomics/Vector_Bionomics_Calculator.R
+++ MASH-dev/DanielCitron/MBITES_vector_bionomics/Vector_Bionomics_Calculator.R
@@ -1,14 +1,28 @@
-require(data.table)
-
 ###
 # Vector_Bionomics_Calculator.R
 #
 # Daniel T. Citron, 5/25/18
 #
 # Scripts for analyzing the JSON output from MBITES event loggers
-# Want to use the simulation output to calculate a variety of bionomic data
+# Want to use the simulation output to calculate a variety of bionomic data, including
+# 1. Vector lifetime distribution
+# 2. Vector blood feeding interval distribution
+# 3. Vector blood feeding events per lifetime distribution
+# 4. Vectorial capacity distribution: 
+#     number of secondary bites that arise following from a single host on a single day
+#
+###
+# Example function call:
+#
+# mosquito.biting.data <- fromJSON(file("/Users/dtcitron/Documents/MASH/MICRO-testing/prettified/run1/mosquito_F_1.json"))
+# rows <- nrow(mosquito.biting.data)-1
+# EIP = 2
+# bionomics.data <- rbindlist(lapply(X=1:rows, vector.bionomics.processor,
+#                             mosquito.biting.data=mosquito.biting.data, EIP=EIP))
 ###
 
+require(data.table)
+require(jsonlite)
 
 ### 
 # preprocess.vector.data
@@ -27,12 +41,11 @@ require(data.table)
 #             probeAndFeed: whether or not the blood feeding attempt was successful - Boolean
 preprocess.vector.data <- function(row.number, mosquito.biting.data = mosquito.biting.data){
   mos.dat.row <- mosquito.biting.data[row.number,]
-  event.times <- unlist(mos.dat.row$time) # this will have to be altered as soon as we have "D" events...
-  #behaviors <- unlist(mos.dat.row$behavior)
+  event.times <- unlist(mos.dat.row$time)
   row.table <- data.table(
     mosID = unlist(mos.dat.row$id),
     birth.time = event.times[1],
-    death.time = event.times[length(event.times)]
+    death.time = event.times[length(event.times)] # last behavior should always be "D" for death
   )
   blood.feeding <- data.table(timeFeed = unlist(mos.dat.row$timeFeed),
                               #siteFeed = unlist(mos.dat.row.ex1$siteFeed), #### FIX ME
@@ -101,7 +114,7 @@ vector.bionomics <- function(mos.data, EIP = EIP){
     bite.sites = mos.data$siteFeed,
     feeding.intervals = feed.intv,
     VC.bites = secondary.bites)
-    )
+  )
 }
 
 ### 
@@ -126,11 +139,119 @@ vector.bionomics.processor <- function(row.number,
   return(vector.bionomics(mos.data, EIP))
 }
 
-vector.bionomics.processor(52, mosquito.biting.data, EIP)
+###
+# combine.bionomics.data
+# Read in and format all bionomics data from an MBITES mosquito biting data file
+# Combines and formats the output data from every mosquito from an MBITES simulation
+###
+# Inputs: file.path.name - File path name - string
+#         EIP - Entomological Incubation Period - integer
+# Outputs: data.table containing bionomic-relevant data
+#             mosquito ID: ID of mosquito- integer
+#             lifetime: life time interval of mosquito - numeric
+#             bite.times: times of primary bites - numeric
+#             bite.hosts: host ID of bitten hosts - integer
+#             bite.sites: site ID where bite occurred - integer
+#             feeding.intervals: time intervals until next bite - numeric
+#             VC.bites: counts of infectious secondary bites arising from each primary bite - integer
+# Example call:
+# bionomics.data <- combine.bionomics.data(filename, EIP = 11)
+###
+combine.bionomics.data <- function(file.path.name, EIP = 11){
+  # read in a JSON file
+  mosquito.biting.data <- fromJSON(file.path.name)
+  # number of rows in the JSON file
+  rows <- nrow(mosquito.biting.data)-1
+  bionomics.data <- rbindlist(lapply(X=1:rows, vector.bionomics.processor,
+                                     mosquito.biting.data=mosquito.biting.data, EIP=EIP))
+  return(bionomics.data)
+}
 
-# To do: 
+
+
+### 
+# lifetimes.distribution.data
+# Calculate histogram data of lifetimes
+# Can be used as an input on a histogram to plot and represent statistics
+###
+# Inputs: bionomics.data, output from combine.bionomics.data
+# Outputs: lifetimes.data - array of mosquito lifetimes - numeric vector
+#
+# Example usage:
+# lifetimes.data <- lifetimes.distribution.data(bionomics.data)
+# hist(lifetimes.data, breaks = c(0:ceiling(max(lifetimes.data)))-.5) # plot a histogram
+###
+lifetimes.distribution.data <- function(bionomics.data){
+  lifetimes.data <- bionomics.data[, .SD[1], by = "mosID"]$lifetime
+  return(lifetimes.data)
+}
+
+
+### 
+# feed.intervals.distribution.data
+# Calculate histogram data of blood feeding intervals
+# Can be used as an input on a histogram to plot and represent statistics
+###
+# Inputs: bionomics.data, output from combine.bionomics.data
+# Outputs: feedint.data - array of intervals between blood feeding events - numeric vector
+#
+# Example usage:
+# feedtimes.data <- feed.intervals.distribution.data(bionomics.data)
+# hist(feedtimes.data, breaks = c(0:ceiling(max(feedtimes.distn)))-.5) # plot a histogram
+###
+feed.intervals.distribution.data <- function(bionomics.data){
+  feedtimes.data <- bionomics.data[!is.na(feeding.intervals)]$feeding.intervals
+  return(feedtimes.data)
+}
+
+
+### 
+# biting.distribution.data
+# Calculate histogram data of bites per mosquito lifetime
+# Can be used as an input on a histogram to plot and represent statistics
+###
+# Inputs: bionomics.data, output from combine.bionomics.data
+# Outputs: numbites.data - array of number of biting events per lifetime - numeric vector
+#
+# Example usage:
+# numbites.data <- biting.distribution.data(bionomics.data)
+# hist(numbites.data, breaks = c(0:ceiling(max(feedtimes.distn)))-.5) # plot a histogram
+###
+biting.distribution.data <- function(bionomics.data){
+  # Count number of bites per mosquito:
+  numbites.data <- bionomics.data[!is.na(bite.times)][,c("mosID","bite.times")][, .N, by = "mosID"]
+  # No bite is listed with NA, so we can add zero-bite mosquitoes as zeroes...
+  numbites.data <- rbind(numbites.data, data.table(mosID = bionomics.data[is.na(bite.times)][,c("mosID")]$mosID, N = 0))$N
+}
+
+
+### 
+# vc.distribution.data
+# Calculate histogram data for vectorial capcity,
+#   specifically the number of secondary bites arising from each primary biting event on each host
+#   NB: This does not require hosts to be infected or infectious, this is only counting the number 
+#       of pairs of events that could potentially lead to parasite transmission.
+#   NB: This can be extended to integrate over all hosts in a particular patch, or over all days,
+#       depending on which specific definition of VC one ascribes to.
+#   NB: The EIP was specified earlier, when bionomics.data was compiled from the raw simulation output
+# Can be used as an input on a histogram to plot and represent statistics
+###
+# Inputs: bionomics.data, output from combine.bionomics.data
+# Outputs: vc.data - array of number of 2ndary bites per host per day - numeric vector
+#
+# Example usage:
+# vc.data <- vc.distribution.data(bionomics.data)
+# hist(vc.data, breaks = c(0:ceiling(max(feedtimes.distn)))-.5) # plot a histogram
+###
+vc.distribution.data <- function(bionomics.data){
+  vc.data <- bionomics.data[!is.na(VC.bites)][, sum(VC.bites), by=.(bite.times, bite.hosts)]$V1
+  return(vc.data)
+}
+
+
+
+###
+# Left to do: 
 # Need to accommodate death event logging (pull, try running again with pretty data)
+# Need to make sure feeding *site* is marked in the json file, along with host
 # Need to accommodate both pretty and mini-formatted data - allow for streaming JSON in (pull, try running without pretty data)
-
-# If necessary: can update the VC calculator to acccomodate differentiating between hosts
-# Right now we are assuming that each mosquito bites a different host, which is wrong.
