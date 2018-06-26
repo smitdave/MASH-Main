64a199340527d9e3309f0832e5950a0244c3b112
smitdave <smitdave@gmail.com>
2018-06-26 16:53:55 -0700

    latest update
    minor changes to states2bionomics.Rmd

--- MASH-dev/DavidSmith/TuningParameters.R
+++ MASH-dev/DavidSmith/TuningParameters.R
@@ -1,72 +1,31 @@
 
 
-PAR1 = list(
+PAR = list(
   # Waiting Times
-  tF = 6/24,   # 30 minutes
+  tF = 8/24, # 30 minutes
   tB = 3/24,   # 3 hours
-  tR = 18/24,  # 18 hours
-  tL = 6/24,   # 30 minutes
+  tR = 30/24,  # 18 hours
+  tL = 8/24, # 30 minutes
   tO = 3/24,   # 1 hour
   
   # Transitions out of F 
-  P_FF = .05,
-  P_FB = .93,
-  
-  # Transitions out of B 
-  P_BF = .1,
-  P_BB = .1, 
-  P_BR = .75,
-  
-  #Transitions out of L 
-  P_LL = .05,
-  P_LO = .93,
-  
-  #Transitions out of O 
-  P_OL = .1,
-  P_OO = .1,
-  P_OB = .4,
-  P_OF = .35,
-  
-  #Transitions out of R
-  P_RF = .05,
-  P_RB = .2,
-  P_RL = .4,
-  P_RO = .35
-) 
-
-P.RR = Psi_RR(PAR1)
-T.RR = T_RR(PAR1)
-T.RR/-log(P.RR)
-T.RR
-
-
-```{r}
-PAR1 = list(
-  # Waiting Times
-  tF = 2/24, # 2 hours
-  tB = 6/24, # 3 hours
-  tR = 1,    # 18 hours
-  tL = 2/24, # 30 minutes
-  tO = 6/24, # 1 hour
-  
-  # Transitions out of F 
   P_FF = .1,
-  P_FB = .88,
+  P_FB = .84,
   
   # Transitions out of B 
-  P_BF = .1,
-  P_BB = .1, 
+  P_BF = .15,
+  P_BB = .05, 
   P_BR = .75,
   
   #Transitions out of L 
-  P_LL = .05,
-  P_LO = .93,
+  P_LL = .1,
+  P_LO = .84,
   
   #Transitions out of O 
-  P_OL = .1,
-  P_OO = .1,
-  P_OB = .4,
-  P_OF = .35,
+  P_OL = .15,
+  P_OO = .05,
+  P_OB = .3,
+  P_OF = .45,
   
   #Transitions out of R
   P_RF = .05,
@@ -74,11 +33,23 @@ PAR1 = list(
   P_RL = .4,
   P_RO = .35
 ) 
-```
 
-```{r}
-P.RR = Psi_RR(PAR)
-T.RR = T_RR(PAR1)
-T.RR/-log(P.RR)
-T.RR
-```
+PAR1=PAR
+PAR1$P_RL = .75
+PAR1$P_RO = 0
+PAR1$P_OB = 0
+PAR1$P_OF = 0.75
+
+PAR2=PAR
+PAR2$P_RL = 0
+PAR2$P_RO = 0.75 
+PAR2$P_OB = 0.75
+PAR2$P_OF = 0
+
+print(c(feed = T_RR(PAR), life=T_RR(PAR)/-log(Psi_RR(PAR)), S=1/(1-Psi_RR(PAR))))
+print(c(feed = T_RR(PAR1), life=T_RR(PAR1)/-log(Psi_RR(PAR1)), S=1/(1-Psi_RR(PAR1))))
+print(c(feed = T_RR(PAR2), life=T_RR(PAR2)/-log(Psi_RR(PAR2)), S=1/(1-Psi_RR(PAR2))))
+
+
+
+
