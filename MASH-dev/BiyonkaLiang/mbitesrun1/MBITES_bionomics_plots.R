f558c67165dd30c35de23fff3d573cec3e87b5e8
Biyonka Liang <Biyonka@MacBook-Pro-2.local>
2018-06-26 20:26:18 -0700

    more plots for MBITES bionomics

--- MASH-dev/BiyonkaLiang/mbitesrun1/MBITES_bionomics_plots.R
+++ MASH-dev/BiyonkaLiang/mbitesrun1/MBITES_bionomics_plots.R
@@ -12,8 +12,12 @@ mosquitos <- fromJSON("mosquito_F_1.json", flatten = TRUE)
 #Takes in a data frame with column called "time" that contains mosquito life events
 #last time is death and first time is birth
 #returns plot of lifespan
-lifespan = function(mosquitos_df) {t = mosquitos_df$time
-  w = sapply(t, FUN = function(v){tail(v, n=1) - v[1]})
+lifespan = function(mosquitos_df) {
+  t = mosquitos_df$time
+  w = sapply(t, FUN = function(v){
+    tail(v, n=1) - v[1]
+    })
+  
   #unlist lifespan values
   w = unlist(w)
   #turn into dataframe for ggplot useage
@@ -21,6 +25,27 @@ lifespan = function(mosquitos_df) {t = mosquitos_df$time
 
   #Mosquito lifespan chart
   ggplot() + geom_histogram(data = e, aes(x), fill = "steelblue", bins = 20) +
-    ggtitle("Mosquito Lifespans") + xlab("Mosquito Lifespan") + ylab("Frequency")}
+    ggtitle("Mosquito Lifespans") + xlab("Days") + ylab("Frequency")}
 
 lifespan(mosquitos)
+
+#Takes in a data frame with column called "bloodHosts" that contains IDs of blood hosts per mosquito
+#returns plot of number of human bloodhosts
+
+humanBloodHost = function(mosquitos_df) {
+  blood_h = mosquitos_df$bloodHosts
+  num_human_host = sapply(blood_h, FUN = function(v){
+    #subset each bloodHost vector to only count humans (id >= 0)
+  length(v[v>= 0])
+  })
+  
+#turn into dataframe for ggplot useage
+e = data.frame(x = num_human_host)
+
+#Mosquito lifespan chart
+ggplot() + geom_histogram(data = e, aes(x), fill = "#fc9272", binwidth = 1) +
+  ggtitle("Human Bloodmeals") + xlab("Count") + ylab("Frequency") +
+  scale_x_continuous(breaks=seq(0, 11, 1))
+}
+
+humanBloodHost(mosquitos)
