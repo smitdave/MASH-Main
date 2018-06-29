d0b721f6f07197e6edbc723e70702d5f480b6850
Biyonka Liang <Biyonka@MacBook-Pro-2.local>
2018-06-28 19:36:37 -0700

    VC plots

--- MASH-dev/BiyonkaLiang/mbitesrun1/MBITES_bionomics_plots.R
+++ MASH-dev/BiyonkaLiang/mbitesrun1/MBITES_bionomics_plots.R
@@ -1,4 +1,3 @@
-rm(list=ls());gc()
 library(spatstat)
 library(truncdist)
 library(viridis)
@@ -8,6 +7,7 @@ library(ggplot2)
 setwd("~/MASH-Main/MASH-dev/BiyonkaLiang/mbitesrun1")
 
 mosquitos <- fromJSON("mosquito_F_1.json", flatten = TRUE)
+human <- fromJSON("human_1.json", flatten = TRUE)
 
 #Takes in a data frame with column called "time" that contains mosquito life events
 #last time is death and first time is birth
@@ -51,21 +51,23 @@ ggplot() + geom_histogram(data = e, aes(x), fill = "#fc9272", binwidth = 1) +
 humanBloodHost(mosquitos)
 
 #Takes in a data frame with column called "bloodHosts" that contains IDs of blood hosts per mosquito
-#subset each elem of time_feed such that it only contains values where corresponding Bloodhost is positive
+#subset each elem of time_feed such that it only contains values
+#where corresponding Bloodhost is positive if human = TRUE. Else, get all intervals
 #returns plot of number of bloodmeal intervals
 
-bloodIntervals = function(mosquitos_df) {
+bloodIntervals = function(mosquitos_df, human = TRUE) {
   blood_h = mosquitos_df$bloodHosts
   time_feed = mosquitos_df$timeFeed
   time_feed_copy = mosquitos_df$timeFeed
-  #get time_feed only where blood_h >= 0
+  if (human == TRUE){
+    #get time_feed only where blood_h >= 0
   for (i in seq(1, length(time_feed), 1)){
     b_elem = blood_h[[i]]
     index = which(b_elem >= 0)
     time_feed_copy[[i]] = time_feed[[i]][index]
   }
-
-  intervals = sapply(time_feed, FUN = function(v){
+}
+  intervals = sapply(time_feed_copy, FUN = function(v){
     if (length(v) == 1) {return (0)}
     diff(v)
   })
@@ -81,3 +83,29 @@ bloodIntervals = function(mosquitos_df) {
 
 bloodIntervals(mosquitos)
 
+
+#the number of pairs of human bites 
+#separated by at least EIP days
+#divided by the number of humans
+# Counts number of diff() of bite_time that are >= EIP (is that right?)
+VC = function(mosquito_df, EIP = 0.5) {
+num_humans = length(human$id)
+
+bites = sapply(mosquito_df$bite_times, FUN = function(v) { 
+  if (length(v) == 1) {return (0)}
+  diff(v)
+})
+
+len_bites = sapply(test, FUN = function(v) { 
+  length(v[v >= EIP])/num_humans
+})
+
+#turn into dataframe for ggplot useage
+e = data.frame(x = t)
+
+ggplot() + geom_histogram(data = e, aes(x), fill = "chartreuse4", bins = 4) +
+  ggtitle("Vectorial Capacity") + xlab("Vectorial Capacity") + ylab("Frequency")
+
+}
+
+VC(mosquitos)
