3e9b654b7c9f75d5127d554c46e7fd9c44ceb5de
slwu89 <slwu89@berkeley.edu>
2018-08-08 12:50:10 -0700

    clean up code

--- MASH-dev/SeanWu/MBITES-Scripts/LANDSCAPE-Trivial.R
+++ MASH-dev/SeanWu/MBITES-Scripts/LANDSCAPE-Trivial.R
@@ -221,34 +221,6 @@ MBDETES$eggs <- MBDETES$cohort[,7]
 MBDETES$bloodmeals <- MBDETES$cohort[,8]
 
 
-# # binomics plots for cohort
-# par(mfrow=c(2,2), mar = c(5,4,2,2))
-#
-# # survival function of cohort (lifespan plot in MBITES)
-# alive_mean <- weighted.mean(tt,alive)
-# plot(tt, alive, type = "l", xlab = "Age (days)", ylab = "Density", main = paste0("Cohort Survival Time (mean: ",round(alive_mean,3),")"),lwd=2,col="steelblue")
-# polygon(c(0, tt), c(0, alive), border=NA, col=adjustcolor("steelblue",alpha.f = 0.5))
-# abline(v = alive_mean,lwd=2.5,lty=2,col="firebrick3")
-#
-# # egg laying rate
-# egg_mean <- weighted.mean(tt[-1],diff(eggs))
-# plot(tt[-1], diff(eggs),type = "l", xlab = "Age (days)", ylab = "Density", main = paste0("Egg Laying Rate (mean: ",round(egg_mean,3),")"),lwd=2,col="steelblue")
-# polygon(c(0, tt[-1]), c(0, diff(eggs)), border=NA, col=adjustcolor("steelblue",alpha.f = 0.5))
-# abline(v = egg_mean,lwd=2.5,lty=2,col="firebrick3")
-#
-# # blood feeding rate
-# blood_mean <- weighted.mean(tt[-1],diff(bloodmeals))
-# plot(tt[-1], diff(bloodmeals),type = "l", xlab = "Age (days)", ylab = "Density", main = paste0("Blood Feeding Rate (mean: ",round(blood_mean,3),")"),lwd=2,col="steelblue")
-# polygon(c(0, tt[-1]), c(0, diff(bloodmeals)), border=NA, col=adjustcolor("steelblue",alpha.f = 0.5))
-# abline(v = blood_mean,lwd=2.5,lty=2,col="firebrick3")
-#
-# # length of feeding (gonotrophic) cycle (blood meal interval in MBITES)
-# plot(tt_pdf[ix], R2R_pdf[ix], type = "l", xlab = "Time (Days)", ylab = "Density",main=paste0("Feeding Cycle Duration (mean: ",round(R2R_mean,3),")"),lwd=2,col="steelblue")
-# polygon(c(0, tt_pdf[ix]), c(0, R2R_pdf[ix]), border=NA, col=adjustcolor("steelblue",alpha.f = 0.5))
-# abline(v = R2R_mean,lwd=2.5,lty=2,col="firebrick3")
-# par(mfrow=c(1,1))
-
-
 ###############################################################################
 # MBDETES vs. MBITES comparison
 ###############################################################################
