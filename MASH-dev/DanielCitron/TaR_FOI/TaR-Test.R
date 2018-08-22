ec608e7da656fcc1e29103342116af219e36da2f
slwu89 <slwu89@berkeley.edu>
2018-08-22 13:51:40 -0700

    dumb mistakes in nloptr

--- MASH-dev/DanielCitron/TaR_FOI/TaR-Test.R
+++ MASH-dev/DanielCitron/TaR_FOI/TaR-Test.R
@@ -75,7 +75,7 @@ eval_g_ineq <- function(theta,pr,psi){
     # FOI constraint 2
     -((theta[3]/theta[2]) - 100),
     # FOI constraint 3
-    -((theta[c(5,6,8,9,10)]/theta[7]) - 500),
+    -((sum(theta[c(5,6,8,9,10)])/theta[7]) - 500),
     # psi constraint 1
     -(psi[2,2] - theta[11] - theta[12]),
     # psi constraint 2
@@ -88,11 +88,13 @@ eval_g_ineq <- function(theta,pr,psi){
 theta_init <- c(rep(1e-2,10),1e-2,1e-2)
 
 nlopt_opts <- list(
-  "algorithm" = "NLOPT_LN_BOBYQA",
-  # "algorithm" = "NLOPT_LN_COBYLA",
-  "xtol_rel"=1.0e-8,
-  "maxeval"=1e4L,
-  "print_level" = 1
+  # global methods
+  # "algorithm" = "NLOPT_GN_ORIG_DIRECT_L",
+  # # local gradient-free methods
+  "algorithm" = "NLOPT_LN_COBYLA",
+  "xtol_rel"=1.0e-10,
+  "maxeval"=1e5L,
+  "print_level" = 2
 )
 
 # call out to nlopt
@@ -114,3 +116,6 @@ psi_hat[2,3] <- opt$solution[12]
 
 # results
 psi_hat %*% h_hat
+
+# results vs data
+cbind(psi_hat %*% h_hat,pr)
