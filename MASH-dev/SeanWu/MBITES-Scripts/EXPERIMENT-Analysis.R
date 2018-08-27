3e0ffb6c3c301241125a63a70359c64ce3fda5ef
slwu89 <slwu89@berkeley.edu>
2018-08-27 16:28:13 -0700

    compute distance PDF and CDF for landscapes

--- MASH-dev/SeanWu/MBITES-Scripts/EXPERIMENT-Analysis.R
+++ MASH-dev/SeanWu/MBITES-Scripts/EXPERIMENT-Analysis.R
@@ -13,191 +13,57 @@
 
 rm(list = ls());gc()
 library(MBITES)
-
+library(viridis)
 
 ###############################################################################
-# load data & calculate bionomics for all runs
+# load processed bionomics
 ###############################################################################
-
-library(jsonlite)
-library(parallel)
-library(lokern)
 
 # where the files can be found
 directory <- "/Users/slwu89/Desktop/mbites/peridomIHME/finals/"
-lscape_dir <- "/Users/slwu89/Desktop/git/MASH-Main/MASH-dev/"
 
-# iterate over all runs
-for(i in 1:26){
+run <- "26"
+MBITES <- readRDS(paste0(directory,"/analysis",run,".rds"))
 
-  cat("processing run ",i,"\n")
 
+
+
+
+par(mfrow=c(1,1))
+
+
+with(MBITES,{
+  plot(surv_tt, surv_s,
+       type = "l", xlab = "Age (Days)", ylab = "Density",
+       main=paste0("MBITES Survival Function (mean: ",round(surv_mean,2),")"),
+       lwd=2,col="firebrick3",xlim=c(0,30))
+  polygon(c(0,surv_tt), c(0,surv_s),
+          border=NA, col=adjustcolor("firebrick3",alpha.f = 0.5))
+  abline(v = surv_mean,lwd=2.5,lty=2,col="firebrick3")
+})
+
+
+colors <- viridis(n = 26,alpha = 0.8)
+
+run <- "1"
+MBITES <- readRDS(paste0(directory,"/analysis",run,".rds"))
+
+with(MBITES,{
+  plot(surv_tt, surv_s,
+       type = "l", xlab = "Age (Days)", ylab = "Density",
+       main=paste0("MBITES Survival Function"),
+       lwd=2,col=colors[1],xlim=c(0,30))
+  abline(v = surv_mean,lwd=2.5,lty=2,col=colors[1])
+})
+
+for(i in 2:26){
+  cat("i: ",i,"\n")
   run <- as.character(i)
-
-  MBITES <- list()
-
-  mosquitos_df <- fromJSON(paste0(directory,"/mosquito_F_",run,".json"), flatten = TRUE)
-  null_m <- which(sapply(mosquitos_df$id,is.null))
-  if(length(null_m)>0){
-    mosquitos_df <- mosquitos_df[-null_m,]
-  }
-
-  humans_df <- fromJSON(paste0(directory,"/human_",run,".json"),flatten = TRUE)
-  null_h <- which(sapply(humans_df$id,is.null))
-  if(length(null_h)>0){
-    humans_df <- humans_df[-null_h,]
-  }
-
-  # lifespan and survival
-  MBITES$surv <- Bionomics_lifespan(mosquitos_df)
-  MBITES$surv_cdf <- ecdf(MBITES$surv$lifespan)
-  MBITES$surv_tt <- seq(from=0,to=max(MBITES$surv$lifespan),by=0.25)
-  MBITES$surv_s <- 1 - MBITES$surv_cdf(MBITES$surv_tt)
-  MBITES$surv_mean <- weighted.mean(MBITES$surv_tt,MBITES$surv_s)
-
-  MBITES$life_mean <- mean(MBITES$surv$lifespan)
-
-  # number of blood hosts
-  MBITES$blood_hosts <- Bionomics_humanBloodHost(mosquitos_df,who = "all")
-  MBITES$blood_hosts_mean <- mean(MBITES$blood_hosts$humanHost)
-
-  # feeding interval
-  MBITES$blood_interval <- Bionomics_bloodIntervals(mosquitos_df,who = "all")
-  MBITES$blood_interval_mean <- mean(MBITES$blood_interval$rest_intervals)
-
-  # vectorial capacity
-  MBITES$vc <- Bionomics_vectorialCapacity(mosquitos = mosquitos_df,humans = humans_df,EIP = 10,spatial = T)
-  MBITES$vc_df <- data.frame(vc=sapply(MBITES$vc,function(x){x$VC}))
-  MBITES$vc_mean <- mean(MBITES$vc_df$vc)
-
-  # egg production
-  MBITES$lifetime_egg <- Bionomics_lifetimeOviposition(mosquitos_df,TRUE)
-  MBITES$lifetime_egg_mean <- mean(MBITES$lifetime_egg$lifetime)
-
-  # egg laying rate
-  MBITES$egg_rate <- Bionomics_ovipositionRate(mosquitos_df)
-  MBITES$egg_rate_mean <- mean(MBITES$egg_rate$ages)
-
-  # blood feeding rate
-  MBITES$blood_rate <- Bionomics_bloodfeedingRate(mosquitos_df)
-  MBITES$blood_rate_mean <- mean(MBITES$blood_rate)
-
-  MBITES$dmat <- as.matrix(read.csv(paste0(lscape_dir,"DavidSmith/MBITES-Demo/dist_",run,".csv"), header = FALSE))
-
-  # spatial dispersion of vectorial capacity
+  MBITES <- readRDS(paste0(directory,"/analysis",run,".rds"))
   with(MBITES,{
-
-    cat("getting pairs of bites ... \n")
-
-    # get pairs of bites
-    spatial_vc_pairs <- lapply(vc,function(x){x$spatialVC})
-    spatial_vc_pairs <- Filter(function(x){length(x)>0},spatial_vc_pairs)
-    spatial_vc_pairs <- do.call(c,spatial_vc_pairs)
-
-    cat("calculating distance matrix of bite pairs ... \n")
-
-    # get distance matrix between sites
-    spatial_vc_dist <- lapply(spatial_vc_pairs,function(x){
-      out <- NULL
-      i <- x$origin
-      for(j in 1:length(x$dest)){
-        out <- append(out,dmat[i,x$dest[j]])
-      }
-      return(out)
-    })
-    spatial_vc_dist <- do.call(c,spatial_vc_dist)
-    spatial_vc_dist <- sort(spatial_vc_dist,decreasing = FALSE)
-
-    cat("binning distance matrix ... \n")
-
-    # discretize into distance bins
-    spatial_vc_bins <- unique(spatial_vc_dist)
-
-    # comparisons of floats
-    fequal <- function(x,y){
-      abs(x-y) <= .Machine$double.eps
-    }
-
-    cat("calculating empirical PMF and CDF ... \n")
-
-    # get empirical PDF by summing stuff in the distance bins (takes awhile, use parallel if you can)
-    spatial_vc_PDF_emp <- mclapply(X = spatial_vc_bins,FUN = function(x,spatial_vc_dist){
-      length(spatial_vc_dist[which(fequal(spatial_vc_dist,x))])
-    },spatial_vc_dist=spatial_vc_dist,mc.cores = detectCores()-2)
-    spatial_vc_PDF_emp <- unlist(spatial_vc_PDF_emp) # mclapply outputs lists; coerce to vector
-    # technically its a PMF so we normalize it
-    spatial_vc_PDF_emp <- spatial_vc_PDF_emp/sum(spatial_vc_PDF_emp)
-
-    # get empirical CDF by preforming a cumulative sum over data points in distance bins
-    spatial_vc_CDF_emp <- cumsum(spatial_vc_PDF_emp)
-
-    cat("smoothing empirical PMF and CDF ... \n")
-
-    # smoothed CDF and PDF
-    MBITES$spatial_vc_CDF_sth <<- spatial_vc_CDF_sth <- glkerns(spatial_vc_bins,spatial_vc_CDF_emp,deriv = 0,korder = 4,x.out=spatial_vc_bins)
-    MBITES$spatial_vc_PDF_sth <<- spatial_vc_PDF_sth <- glkerns(spatial_vc_bins,spatial_vc_CDF_emp,deriv = 1,korder = 3,x.out=spatial_vc_bins)
-
-    MBITES$spatial_vc_CDF_emp <<- spatial_vc_CDF_emp
-    MBITES$spatial_vc_PDF_emp <<- spatial_vc_PDF_emp
-    cat("done! \n")
+    lines(surv_tt, surv_s,
+         lwd=2,col=colors[as.integer(i)])
+    abline(v = surv_mean,lwd=2.5,lty=2,col=colors[as.integer(i)])
   })
 
-  # spatial dispersion of egg batches
-  # spatial egg dispersion
-  with(MBITES,{
-
-    cat("getting pairs of oviposition events ... \n")
-
-    # get pairs of eggs
-    spatial_egg_pairs <- Filter(f = function(x){
-      !(is.nan(x$natal) && is.nan(x$dest))
-    },x = lifetime_egg$dispersion)
-
-    cat("calculating distance matrix of oviposition pairs ... \n")
-
-    # spatial egg dispersion
-    spatial_egg_pairs <- lapply(spatial_egg_pairs,function(x){
-      out <- NULL
-      i <- x$natal
-      for(j in 1:length(x$dest)){
-        out <- append(out,dmat[i,x$dest[j]])
-      }
-      return(out)
-    })
-    spatial_egg_dist <- do.call(c,spatial_egg_pairs)
-    spatial_egg_dist <- sort(spatial_egg_dist,decreasing = FALSE)
-
-    spatial_egg_bins <- unique(spatial_egg_dist)
-
-    cat("calculating empirical PMF and CDF ... \n")
-
-    # get empirical PDF by summing stuff in the distance bins (takes awhile, use parallel if you can)
-    spatial_egg_PDF_emp <- mclapply(X = spatial_egg_bins,FUN = function(x,spatial_egg_dist){
-      length(spatial_egg_dist[which(fequal(spatial_egg_dist,x))])
-    },spatial_egg_dist=spatial_egg_dist,mc.cores = detectCores()-2)
-    spatial_egg_PDF_emp <- unlist(spatial_egg_PDF_emp) # mclapply outputs lists; coerce to vector
-    # technically its a PMF so we normalize it
-    spatial_egg_PDF_emp <- spatial_egg_PDF_emp/sum(spatial_egg_PDF_emp)
-
-    # get empirical CDF by preforming a cumulative sum over data points in distance bins
-    spatial_egg_CDF_emp <- cumsum(spatial_egg_PDF_emp)
-
-    cat("smoothing empirical PMF and CDF ... \n")
-
-    # smoothed CDF and PDF
-    MBITES$spatial_egg_CDF_sth <- glkerns(spatial_egg_bins,spatial_egg_CDF_emp,deriv = 0,korder = 4,x.out=spatial_egg_bins)
-    MBITES$spatial_egg_PDF_sth <- glkerns(spatial_egg_bins,spatial_egg_CDF_emp,deriv = 1,korder = 3,x.out=spatial_egg_bins)
-
-    MBITES$spatial_egg_CDF_emp <<- spatial_egg_CDF_emp
-    MBITES$spatial_egg_PDF_emp <<- spatial_egg_PDF_emp
-    cat("done! \n")
-  })
-
-  outfile <- paste0(directory,"/analysis",run,".rds")
-  cat("writing out to: ",outfile,"\n")
-  saveRDS(object = MBITES,file = outfile,compress = TRUE)
-
-  rm(MBITES,mosquitos_df,humans_df);gc()
-  cat("\n")
 }
-
--- MASH-dev/SeanWu/MBITES-Scripts/LANDSCAPE-Distributions.R
+++ MASH-dev/SeanWu/MBITES-Scripts/LANDSCAPE-Distributions.R
@@ -197,3 +197,129 @@ for(i in 2:nsite){
   lines(sitesKern[[i]]$CDF_sth$x,sitesKern[[i]]$CDF_sth$y,col=adjustcolor("firebrick3",alpha),lwd=3)
 }
 lines(sitesKern[[i]]$CDF_sth$x,meanCDF,col=grey(level = 0.1),lwd=3)
+
+
+
+
+
+
+###############################################################################
+# Calculate & process movement kernels for all sites and landscapes
+###############################################################################
+
+library(parallel)
+library(lokern)
+
+# comparisons of floats
+fequal <- function(x,y){
+  abs(x-y) <= .Machine$double.eps
+}
+
+# where the files can be found
+directory <- "/Users/slwu89/Desktop/git/MASH-Main/MASH-dev/"
+
+for(i in 1:26){
+
+  KERN <- list()
+
+  cat("calculating movement distributions for landscape ",i,"\n")
+
+  run <- as.character(i)
+
+  dist_mat <- as.matrix(read.csv(paste0(directory,"DavidSmith/MBITES-Demo/dist_",run,".csv"), header = FALSE))
+  kernel <- as.matrix(read.csv(paste0(directory,"DavidSmith/MBITES-Demo/movement_",run,".csv"), header = FALSE))
+  kernel <- kernel/rowSums(kernel)
+
+  KERN$dist <- as.vector(dist_mat)
+  KERN$prob <- as.vector(kernel)
+
+  # get rid of diagonal of dist matrix (mosy has no self loops)
+  zeros <- which(fequal(KERN$dist,0))
+
+  KERN$dist <- KERN$dist[-zeros]
+  KERN$prob <- KERN$prob[-zeros]
+
+  # sort in increasing distance
+  ord <- order(KERN$dist)
+
+  KERN$dist <- KERN$dist[ord]
+  KERN$prob <- KERN$prob[ord]
+
+  # make distance bins
+  KERN$distance_bins <- unique(KERN$dist)
+
+  # get empirical PDF by summing stuff in the distance bins (takes awhile, use parallel if you can)
+  KERN$PDF_emp <- mclapply(X = KERN$distance_bins,FUN = function(x,prob,dist){
+    sum(prob[which(fequal(dist,x))])
+  },prob=KERN$prob,dist=KERN$dist,mc.cores = detectCores()-2)
+  KERN$PDF_emp <- unlist(KERN$PDF_emp) # mclapply outputs lists; coerce to vector
+
+  # technically its a PMF so we normalize it
+  KERN$PDF_emp <- KERN$PDF_emp/sum(KERN$PDF_emp)
+
+  # get empirical CDF by preforming a cumulative sum over data points in distance bins
+  KERN$CDF_emp <- cumsum(KERN$PDF_emp)
+
+  # smoothed CDF and PDF
+  KERN$CDF_sth <- glkerns(KERN$distance_bins,KERN$PDF_emp,deriv = 0,korder = 4,x.out=KERN$distance_bins)
+  KERN$PDF_sth <- glkerns(KERN$distance_bins,KERN$CDF_emp,deriv = 1,korder = 3,x.out=KERN$distance_bins)
+
+  cat("calculating movement distributions for all sites\n")
+
+  n <- nrow(dist)
+
+  KERN$sites <- vector("list",n)
+
+  pb <- txtProgressBar(1,n)
+  for(j in 1:n){
+
+    # distance and probabilities for site i
+    dist <- as.vector(dist_mat[j,])
+    prob <- as.vector(kernel[j,])
+
+    # get rid of zero distance elements (have probability 0 anyway)
+    zeros <- which(fequal(dist,0))
+
+    dist <- dist[-zeros]
+    prob <- prob[-zeros]
+
+    # sort by increasing distance
+    ord <- order(dist)
+
+    dist <- dist[ord]
+    prob <- prob[ord]
+
+    # get empirical PDF by summing stuff in the distance bins (takes awhile, use parallel if you can)
+    PDF_emp <- mclapply(X = KERN$distance_bins,FUN = function(x,prob,dist){
+      sum(prob[which(dist %in% x)])
+    },prob=prob,dist=dist,mc.cores = detectCores()-2)
+    PDF_emp <- unlist(PDF_emp) # mclapply outputs lists; coerce to vector
+
+    # technically its a PMF so we normalize it
+    PDF_emp <- PDF_emp/sum(PDF_emp)
+
+    # get empirical CDF by preforming a cumulative sum over data points in distance bins
+    CDF_emp <- cumsum(PDF_emp)
+
+    # smoothed CDF and PDF
+    CDF_sth <- glkerns(KERN$distance_bins,PDF_emp,deriv = 0,korder = 4,x.out=KERN$distance_bins)
+    PDF_sth <- glkerns(KERN$distance_bins,CDF_emp,deriv = 1,korder = 3,x.out=KERN$distance_bins)
+
+    # put it in the container for this site
+    KERN$sites[[j]]$PDF_emp <- PDF_emp
+    KERN$sites[[j]]$CDF_emp <- CDF_emp
+    KERN$sites[[j]]$CDF_sth <- CDF_sth
+    KERN$sites[[j]]$PDF_sth <- PDF_sth
+
+    setTxtProgressBar(pb,j)
+  }
+  setTxtProgressBar(pb,j+1)
+
+  outfile <- paste0(directory,"DavidSmith/MBITES-Demo/processed_kernels_",run,".rds")
+
+  cat("writing out to: ",outfile,"\n")
+  saveRDS(object = KERN,file = outfile,compress = TRUE)
+
+  rm(KERN);gc()
+  cat("\n")
+}
