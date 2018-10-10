60c0497b5f79c38854d94b103f0bdaccd1c7b326
smitdave <smitdave@gmail.com>
2018-10-10 11:59:15 -0700

    updated file structure

---
+++ MASH-dev/DavidSmith/Deprecated/MBITES-Bouts.R
@@ -0,0 +1,383 @@
+###############################################################################
+#       __  ___      ____  _____________________
+#      /  |/  /     / __ )/  _/_  __/ ____/ ___/
+#     / /|_/ /_____/ __  |/ /  / / / __/  \__ \
+#    / /  / /_____/ /_/ // /  / / / /___ ___/ /
+#   /_/  /_/     /_____/___/ /_/ /_____//____/
+#
+#   MASH-MICRO
+#   M-BITES: Bouts
+#   MASH-MICRO Team
+#   September 2017
+#
+###############################################################################
+
+#################################################################
+#
+# M-BITES: Bouts
+#
+#################################################################
+
+#################################################################
+# M-BITES: The Generic Search Bout is called if search=TRUE
+#################################################################
+
+#' M-BITES: Generic Search Bout \code{MosquitoFemale}
+#'
+#' The generic search bout:
+#'   1) call moveMe() to find a new site;
+#'   2a) a mosquito leaves to initiate a new search;
+#'   2b) a mosquito stays (a success) and attenpts to land.
+#'
+mbites_boutSearch <- function(){
+  self$moveMe()
+  pr_success = switch(private$state,
+    B = private$FemalePopPointer$get_MBITES_PAR("Bs_succeed")
+    O = private$FemalePopPointer$get_MBITES_PAR("Os_succeed")
+    S = private$FemalePopPointer$get_MBITES_PAR("Ss_succeed")
+    M = private$FemalePopPointer$get_MBITES_PAR("Ms_succeed")
+  )
+  if(runif(1) < pr_success){
+    private$search = FALSE      # the next bout will be an attempt
+    private$lspot = "l"         # initialize restingSpot()
+  }
+  #TO DO: Implement area repellants
+}
+
+
+#################################################################
+# M-BITES: Blood Feeding Attempt Bout :: B
+#################################################################
+
+#' M-BITES: Blood Feeding Attempt Bout (B) \code{MosquitoFemale}
+#'
+#' The blood feeding attempt bout has the following structure:
+#'
+#'    1) choose a host;
+#'    2a) if the host is human, simulate a human encounter
+#'    2b) if the host is not human, simulate a zoonotic encounter
+#'    2c) if the mosquito chooses a trap, simulate the outcome
+#'        NOTE: a CDC light trap is one kind of trap
+#'    2d) a null host is for a failed attempt
+#'
+#' NOTE: host encounters are found in MBITES-HostEncounter.R
+#'       bloodtrap() is found in ...
+#'
+mbites_boutB <- function(){
+  # check success
+  if(runif(1) < private$FemalePopPointer$get_MBITES_PAR("B_succeed")){
+    self$chooseHost() # MBITES-ChooseHost.R
+  } else {
+    private$hostID = 0L
+  }
+
+  if(private$hostID > 0){
+    self$humanEncounter() # MBITES-HostEncounter.R
+  } else if(private$hostID == -1){
+    self$zooEncounter() # MBITES-HostEncounter.R
+  } else if(private$hostID == -2){
+    self$bloodtrap()
+  } else if(private$hostID == 0){
+    return(NULL)
+  } else {
+    stop("illegal hostID value")
+  }
+}
+
+mbites_bloodtrap <-function(){
+   # move me!
+   # write me!
+}
+
+#################################################################
+# M-BITES: Egg Laying Attempt Bout :: O
+#################################################################
+
+#' M-BITES: Egg Laying Attempt Bout (O) \code{MosquitoFemale}
+#'
+#' The egg laying attempt bout has the following structure:
+#'
+#'    1) choose a habitat;
+#'    2a) lay eggs in a habitat
+#'    2b) if a mosuqito chooses an ovitrap, simulate the outcome
+#'    2c) a null habitat is for a failed attempt
+#'
+#'
+#'
+mbites_boutO <- function(){
+  if(runif(1) < private$FemalePopPointer$get_MBITES_PAR("O_succeed")){
+    self$chooseHabitat() # MBITES-EggLaying.R
+  } else {
+    private$habitatID = 0L
+  }
+
+  if(private$habitatID > 0){
+    self$layEggs() # MBITES-EggLaying.R
+  } else if(private$habitatID == -1){
+    #print("OVI Present")
+    self$ovitrap()
+  } else if(private$habitatID == 0){
+    return(NULL)
+  } else {
+    stop("illegal hostID value")
+  }
+}
+
+#################################################################
+# M-BITES: Sugar Feeding Attempt Bout :: S
+#################################################################
+
+#' M-BITES: Sugar Feeding Bout (S) \code{MosquitoFemale}
+#'
+#'
+#' The sugar feeding attempt bout has the following structure:
+#'
+#'    1) choose a sugar source;
+#'    2a) take a sugar meal
+#'    2b) if a mosuqito chooses an atsb, simulate the outcome
+#'    2c) a null source is for a failed attempt
+#'
+#'  * This method is bound to \code{MosquitoFemale$boutS()}.
+#'
+mbites_boutS <- function(){
+  if(runif(1) < private$FemalePopPointer$get_MBITES_PAR("S_succeed")){
+    self$chooseSugarSource()
+  } else {
+    private$sugarID = 0L
+  }
+
+  if(private$sugarID > 0){
+    self$sugarMeal()   # MBITES-Energetics.R
+  } else if(private$sugarID == -1){
+    self$atsb()
+  } else if(private$sugarID == 0){
+    return(NULL)
+  } else {
+    stop("illegal sugarID value")
+  }
+}
+
+
+mbites_atsb <- function(){
+# write me!
+}
+
+#################################################################
+# M-BITES: Mating Bout :: M
+#################################################################
+
+#' M-BITES: Mating Bout (M) \code{MosquitoFemale}
+#'
+#' A mosquito performs mating bout.
+#'
+#'    1) choose a mate from the swarmingQ ;
+#'    2a) mate
+#'    2b) the swarm has been sprayed.
+#'    2c) a null mate is for a failed attempt
+#
+#'  * This method is bound to \code{MosquitoFemale$boutM()}.
+#'
+mbites_boutM <- function(){
+  if(runif(1) < private$FemalePopPointer$get_MBITES_PAR("M_succeed")){
+    self$chooseMate()
+  } else {
+    private$mateID = 0L
+  }
+
+  if(private$mateID > 0){
+    #self$mating()
+    private$mated = TRUE
+  } else if(private$mateID == -1){
+    self$swarmSpray()
+  } else if(private$mateID == 0){
+    return(NULL)
+  } else {
+    stop("illegal mateID value")
+  }
+
+
+  #### SwarmSpray #############################################################################################
+  swarmSpray=private$LandscapePointer$get_MatingSites(private$locNow)$get_swarmSpray()
+  if(is.null(swarmSpray)==FALSE){
+      private$stateNew = swarmSpray$mosquitoKillEncounter(private$stateNew,interventionType="SwarmSpray")
+      private$lspot = swarmSpray$mosquitoRepelEncounter(private$lspot,interventionType="SwarmSpray")
+  }
+
+}
+
+
+#################################################################
+# M-BITES: Generic Bout
+#################################################################
+
+#' M-BITES: One Bout \code{MosquitoFemale}
+#'
+#' Mosquito behavior has a finite set of states (state space of model), within which there are certain biological functions that are always evaluated.
+#' A bout is the actions taken by a mosquito between a launch and landing; \code{mbites_oneBout} handles all the biological imperatives that occur during a bout,
+#' while specialized bout action methods handle the events that occur due to the intent of the mosquito during the bout. The two classes of bouts are SEARCH and ATTEMPT.
+#' Search bouts are similar enough that there is a generic version of it.
+#'  * \code{\link{mbites_boutB}}: blood feeding attempt bout
+#'  * \code{\link{mbites_boutO}}: egg laying attempt bout
+#'  * \code{\link{mbites_boutS}}: sugar feeding attempt bout
+#'  * \code{\link{mbites_boutM}}: mating bout
+#'  * \code{\link{mbites_boutSearch}}: a search bout for any
+#'
+#' The generic bout runs necessary updates of timing, state, survival, energetics, and queue checks prior to calling the nested
+#' specific bout action, and checks that the mosquito is alive/active before calling the bout. It updates \code{tNext} and \code{stateNew}.
+#'
+#' This corresponds to the following Gillespie-style algorithm:
+#'
+#' 1. tNow is set to tNext from previous bout
+#' 2. Launch: an attempt or search bout
+#' 3. Land: restingSpot is called to find a microsite for resting
+#' 4. Rest: update the mosquito's state
+#' 5. Log events
+#'
+#'  * This method is bound to \code{MosquitoFemale$oneBout()}.
+#'
+mbites_oneBout <- function(){
+
+  # tNext is when the next launch will occur.
+  # a bout is not executed if contingent events
+  # have not been updated.
+  private$tNow = private$tNext
+
+  # LAUNCH & TRY
+  if(private$search==TRUE){
+    # The Generic Search Bout
+    self$boutSearch()
+  } else {
+    # The Attempt Bouts
+    switch(private$state,
+      B = {self$boutB()},
+      O = {self$boutO()},
+      M = {self$boutM()},
+      S = {self$boutS()},
+      {stop(cat("illegal behavioral state: ",private$state,"\n",sep=""))}
+    )
+  }
+
+  # LAND
+  self$restingSpot() # MBITES-Restingspot.R
+                     # NOTE: restingSpot() can set leave=TRUE
+  # REST
+  self$updateState()
+
+  # log history
+  private$history$historyTrack(privateEnv = private, alive = self$isAlive())
+}
+
+#################################################################
+# M-BITES: Update State
+#################################################################
+
+#' M-BITES: Update the Behavioral State at the End of a Bout for \code{\link{MosquitoFemale}}
+#'
+#' After landing, during the resting period, the mosquito's
+#' behavioral state and other state variables are updated.
+#'
+#' M-BITES checks the state variables during the resting
+#' period to determine what the next state will be. There is
+#' a natural hierarchy: a dead mosquito can never be revived;
+#' a starved mosquito will seek sugar; a gravid mosquito will
+#' tend to lay eggs, though it might decide to top up with
+#' blood; if nothing else, a mosquito will seek blood.
+#'
+mbites_updateState <- function(){
+  self$energetics()    # MBITES-Energetics.R
+  self$survival()      # MBITES-Survival.R
+
+  # The states in priority order
+  if(private$state == 'D'){
+    private$state = 'D'}else{
+    if(private$starved == TRUE){
+      private$state = 'S'}else{
+      if(private$gravid == TRUE) {
+        self$checkRefeed()}else{     #MBITES-Oogenesis.R
+          private$state = 'B'}}}
+
+  # The states in priority order
+  self$timing()  # MBITES-Timing.R
+                 # NOTE: timing() handles the transition to mating.
+                 #      if state='B', it can be reset to
+                 #      state='M'
+
+  # if there are no resources of the required type present, set
+  # search = TRUE
+  self$checkForResources()
+}
+
+#################################################################
+# M-BITES: Check to see if a search is required
+#################################################################
+
+#' M-BITES: If the required resource is not here, initiate a search \code{\link{MosquitoFemale}}
+#'
+#' After running a bout, this code checks the mosquito's
+#' behavioral state agains the local resources to see if a search is
+#' required
+#'
+mbites_checkForResources <- function(){
+    switch(private$state,
+      B = {private$search=TRUE},
+      O = {self$OvipositSearchCheck()},
+      M = {private$search=TRUE},
+      S = {private$search=TRUE},
+      {stop(cat("illegal behavioral state: ",private$state,"\n",sep=""))}
+    )
+}
+
+#' M-BITES: Check for Oviposit Search Bout \code{MosquitoFemale}
+#'
+#' During a resting bout \code{\link{mbites_boutR}}, the mosquito checks if she is in a
+#'  * this method is bound to \code{MosquitoFemale$OvipositSearchCheck}
+mbites_OvipositSearchCheck <- function(){
+  if(private$pSetNow=="f"){
+    # check for peri-domestic breeding sites
+    if(!is.null(private$LandscapePointer$get_FeedingSites(private$locNow)$get_periDomestic())){
+      # has peri-domestic breeding site
+      private$stateNew = "O"
+      private$periDomestic = TRUE
+    } else {
+      # does not have peri-domestic breeding site
+      private$search = TRUE
+    }
+  } else {
+    private$search=TRUE
+  }
+}
+
+#################################################################
+# M-BITES: Simulation
+#################################################################
+
+#' M-BITES: Run Simulation for \code{\link{MosquitoFemale}}
+#'
+#' Run the M-BITES life cycle simulation algorithm while alive and has not overrun time in enclosing \code{\link{Tile}}.
+#' This method calls \code{\link{mbites_oneBout}} to simulate each life stage.
+#'  * This method is bound to \code{MosquitoFemale$MBITES()}.
+#'
+mbites_oneMosquito_MBITES <- function(){
+
+  # run algorithm while alive and has not overrun tile time
+  while(private$tNext < private$TilePointer$get_tNow() & private$state != 'D'){
+    self$oneBout()
+  }
+
+  # if mosquito is dead output data if asked and remove it from the enclosing storage object
+  if(private$state == 'D'){
+    self$writeAndDelete(conHist = private$TilePointer$get_FemaleHistoryCon(), conPath = private$TilePointer$get_MosquitoPathogenCon())
+  }
+
+}
+
+#' M-BITES: Run Simulation for \code{\link{MosquitoPopFemale}}
+#'
+#' Run the M-BITES life cycle simulation algorithm while alive and has not overrun time in enclosing \code{\link{Tile}}.
+#' This method calls \code{\link{mbites_oneMosquito_MBITES}} one each living mosquito sequentially.
+#'  * This method is bound to \code{MosquitoPopFemale$MBITES()}.
+#'
+mbites_Pop_MBITES <- function(){
+  private$pop$apply(tag="MBITES",returnVal=FALSE)
+
+}
---
+++ MASH-dev/DavidSmith/Deprecated/MBITES-Setup.R
@@ -0,0 +1,371 @@
+###############################################################################
+#       __  ___      ____  _____________________
+#      /  |/  /     / __ )/  _/_  __/ ____/ ___/
+#     / /|_/ /_____/ __  |/ /  / / / __/  \__ \
+#    / /  / /_____/ /_/ // /  / / / /___ ___/ /
+#   /_/  /_/     /_____/___/ /_/ /_____//____/
+#
+#   MASH-MICRO
+#   M-BITES: Setup
+#   MASH-MICRO Team
+#   September 2017
+#
+###############################################################################
+
+#' M-BITES: Initialize Additional Methods & Fields in \code{\link{MosquitoPopFemale}} and \code{\link{MosquitoFemale}}
+#'
+#' Initialize M-BITES lifecycle model (complex form). This model includes:
+#'  * Blood Feeding Search Bout: see \code{\link{mbites_boutF}}
+#'  * Blood Feeding Attempt Bout: see \code{\link{mbites_boutB}}
+#'  * Post-Prandial Resting Bout: see \code{\link{mbites_boutR}}
+#'  * Egg Laying Search Bout: see \code{\link{mbites_boutL}}
+#'  * Egg Laying Attempt Bout: see \code{\link{mbites_boutO}}
+#'  * Sugar Feeding Attempt Bout: see \code{\link{mbites_boutS}}
+#'  * Mating Bout: see \code{\link{mbites_boutM}}
+#'
+#' @param overwrite overwrite existing methods
+#' @param SUGAR enable/disable sugar feeding and sugar energetics
+#' @param MATE enable/disable mating behavior
+#' @param aquaModule character switch in \code{"emerge","EL4P"}, selects aquatic ecology module
+#' @param timing character switch in \code{"exponential","gamma"}, selects waiting time distribution from \code{\link{mbites_timingExponential}} or \code{\link{mbites_timingGamma}}
+#' @param tattering character switch in \code{"mean","exact"}, mean will sample wing tattering from \code{\link{mbites_WingTattering}} regardless of flight distance
+#' @param energy character switch in \code{"mean","exact"}, mean will sample energy consumption
+#' @param batchSize character switch in \code{"bms","norm"} for egg batch sizes dependent on bloodmeal size or normally distributed
+#' @param eggMatT character switch in \code{"off","norm"} for egg batch maturation time turned off or normally distributed
+#'
+#'
+#' @return writes methods to \code{\link{MosquitoFemale}} and \code{\link{MosquitoMale}} classes.
+#' @export
+MBITES.Setup <- function(
+  overwrite = TRUE,
+  SUGAR = TRUE,
+  MATE = TRUE,
+  aquaModule = "emerge",
+  timing = "exponential",
+  tattering = "mean",
+  energy = "mean",
+  batchSize = "bms",
+  eggMatT = "off"
+){
+
+  # alert user
+  cat("initializing M-BITES\n",sep="")
+
+  ##############################################################
+  # MBITES-Energetics.R
+  ##############################################################
+
+  MosquitoFemale$set(which = "public",name = "BloodMeal",
+            value = mbites_BloodMeal, overwrite = overwrite
+  )
+
+  ##############################################################
+  # MBITES-Survival.R
+  ##############################################################
+
+  MosquitoFemale$set(which = "public",name = "get_surviveFlightProb",
+            value = mbites_get_surviveFlightProb, overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "get_restHaz",
+            value = mbites_get_restHaz, overwrite = overwrite
+  )
+
+
+  ##############################################################
+  # MBITES-Timing.R
+  ##############################################################
+
+  switch(timing,
+    exponential = {
+      MosquitoFemale$set(which = "public",name = "timing",
+                value = mbites_timingExponential, overwrite = overwrite
+      )
+    },
+    gamma = {
+      MosquitoFemale$set(which = "public",name = "timing",
+                value = mbites_timingGamma, overwrite = overwrite
+      )
+    },
+    {stop(cat("unrecognized entry for timing, expected character in 'exponential', 'gamma', 'weibull', got: ",timing,"\n",sep=""))}
+  )
+
+  ##############################################################
+  # MBITES-Bouts.R
+  ##############################################################
+
+  MosquitoFemale$set(which = "public",name = "get_MySiteType",
+            value = mbites_get_MySiteType, overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "get_WTS",
+            value = mbites_get_WTS, overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "newSpot",
+            value = mbites_newSpot, overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "enterHouse",
+            value = mbites_enterHouse, overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "restingSpot",
+            value = mbites_restingSpot, overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "searchFail",
+            value = mbites_searchFail, overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "boutF",
+            value = mbites_boutF, overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "boutB",
+            value = mbites_boutB, overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "boutR",
+            value = mbites_boutR, overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "OvipositSearchCheck",
+            value = mbites_OvipositSearchCheck, overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "boutL",
+            value = mbites_boutL, overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "boutO",
+            value = mbites_boutO, overwrite = overwrite
+  )
+
+  switch(aquaModule,
+    emerge = {
+      MosquitoFemale$set(which = "public",name = "layEggs",
+                value = mbites_layEggs_Emerge, overwrite = overwrite
+      )
+    },
+    EL4P = {
+      MosquitoFemale$set(which = "public",name = "layEggs",
+                value = mbites_layEggs_EL4P, overwrite = overwrite
+      )
+    },
+    {stop("unrecognized Aquatic Ecology module")}
+  )
+
+
+  if(SUGAR){
+    MosquitoFemale$set(which = "public",name = "boutS",
+              value = mbites_boutS, overwrite = overwrite
+    )
+  }
+
+  if(MATE){
+    MosquitoFemale$set(which = "public",name = "boutM",
+              value = mbites_boutM, overwrite = overwrite
+    )
+  }
+
+  MosquitoFemale$set(which = "public",name = "oneBout",
+            value = mbites_oneBout, overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "MBITES",
+            value = mbites_oneMosquito_MBITES, overwrite = overwrite
+  )
+
+  MosquitoPopFemale$set(which = "public",name = "MBITES",
+            value = mbites_Pop_MBITES, overwrite = overwrite
+  )
+
+  #################################################################
+  # MBITES-HostEncounter.R
+  #################################################################
+
+  MosquitoFemale$set(which = "public",name = "humanEncounter",
+            value = mbites_humanEncounter,
+            overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "zooEncounter",
+            value = mbites_zooEncounter,
+            overwrite = overwrite
+  )
+
+  ##############################################################
+  # MBITES-ChooseHost.R
+  ##############################################################
+
+  MosquitoFemale$set(which = "public",name = "chooseHost",
+            value = mbites_chooseHost,
+            overwrite = overwrite
+  )
+
+  ##############################################################
+  # MBITES-ChooseMate.R
+  ##############################################################
+
+  MosquitoFemale$set(which = "public",name = "chooseMate",
+            value = mbites_chooseMate,
+            overwrite = overwrite
+  )
+
+  ##############################################################
+  # MBITES-Auxiliary.R
+  ##############################################################
+
+  MosquitoFemale$set(which = "public",name = "isAlive",
+            value = mbites_isAlive,
+            overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "isActive",
+            value = mbites_isActive,
+            overwrite = overwrite
+  )
+
+  MosquitoMale$set(which = "public",name = "isAlive",
+            value = mbites_isAlive,
+            overwrite = overwrite
+  )
+
+  MosquitoMale$set(which = "public",name = "isActive",
+            value = mbites_isActive,
+            overwrite = overwrite
+  )
+
+  ##############################################################
+  # MBITES-Energetics.R
+  ##############################################################
+
+  MosquitoFemale$set(which = "public",name = "sugarEnergetics",
+            value = mbites_sugarEnergetics, overwrite = overwrite
+  )
+
+  if(energy=="mean"){
+
+    MosquitoFemale$set(which = "public",name = "flightEnergetics",
+              value = mbites_flightEnergetics_Mean, overwrite = overwrite
+    )
+
+  } else if(energy=="exact"){
+
+    MosquitoFemale$set(which = "public",name = "flightEnergetics",
+              value = mbites_flightEnergetics_Exact, overwrite = overwrite
+    )
+
+  } else {
+    stop(cat("unrecognized entry for energy, expected character in 'mean', 'exact', got: ",energy,"\n",sep=""))
+  }
+
+  MosquitoFemale$set(which = "public",name = "queueSugarBout",
+            value = mbites_queueSugarBout,
+            overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "pEnergySurvival",
+            value = mbites_pEnergySurvival,
+            overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "pSugarBout",
+            value = mbites_pSugarBout,
+            overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "rBloodMealSize",
+            value = mbites_rBloodMealSize,
+            overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "pOverFeed",
+            value = mbites_pOverFeed,
+            overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "pReFeed",
+            value = mbites_pReFeed,
+            overwrite = overwrite
+  )
+
+  # rBatchSize
+  switch(batchSize,
+    bms = {
+      MosquitoFemale$set(which = "public",name = "rBatchSize",
+                value = mbites_rBatchSizeBms,
+                overwrite = overwrite
+      )
+    },
+    norm = {
+      MosquitoFemale$set(which = "public",name = "rBatchSize",
+                value = mbites_rBatchSizeNorm,
+                overwrite = overwrite
+      )
+    },
+    {stop("unrecognized entry for batchSize")}
+  )
+
+  # rEggMaturationTime
+  switch(eggMatT,
+    off = {
+      MosquitoFemale$set(which = "public",name = "rEggMaturationTime",
+                value = mbites_rEggMaturationTimeOff,
+                overwrite = overwrite
+      )
+    },
+    norm = {
+      MosquitoFemale$set(which = "public",name = "rEggMaturationTime",
+                value = mbites_rEggMaturationTimeNorm,
+                overwrite = overwrite
+      )
+    },
+    {stop("unrecognized entry for eggMatT")}
+  )
+
+
+  ##############################################################
+  # MBITES-Survival.R
+  ##############################################################
+
+  MosquitoFemale$set(which = "public",name = "surviveFlight",
+            value = mbites_surviveFlight,
+            overwrite = overwrite
+  )
+
+  if(tattering=="mean"){
+
+    MosquitoFemale$set(which = "public",name = "WingTattering",
+              value = mbites_WingTattering_Mean, overwrite = overwrite
+    )
+
+  } else if(tattering=="exact"){
+
+    MosquitoFemale$set(which = "public",name = "WingTattering",
+              value = mbites_WingTattering_Exact, overwrite = overwrite
+    )
+
+  } else {
+    stop(cat("unrecognized entry for tattering, expected character in 'mean', 'exact', got: ",energy,"\n",sep=""))
+  }
+
+
+  MosquitoFemale$set(which = "public",name = "pTatter",
+            value = mbites_pTatter,
+            overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "pSenesce",
+            value = mbites_pSenesce,
+            overwrite = overwrite
+  )
+
+  MosquitoFemale$set(which = "public",name = "surviveResting",
+            value = mbites_surviveResting,
+            overwrite = overwrite
+  )
+
+
+}
