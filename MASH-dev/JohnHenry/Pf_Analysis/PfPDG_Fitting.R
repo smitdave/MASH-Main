a8f7c75ecf3c2740315fe94fd0bdb55bdf89f95b
John Henry <henry529@ihme.washington.edu>
2019-03-07 12:06:44 -0800

    update vivax analysis
    include gametocyte patterns, transmission efficiency
    moved Pf analysis into separate folder

---
+++ MASH-dev/JohnHenry/Pf_Analysis/PfPDG_Fitting.R
@@ -0,0 +1,522 @@
+library(readxl)
+library(vioplot)
+library(matrixStats)
+library(stinepack)
+
+MT_PT_NP <- read_excel("~/GitHub/MASH-Main/MASH-dev/JohnHenry/PDG/MT_PT_NP.xlsx")
+M = as.matrix(MT_PT_NP)
+
+MT_GT_NP <- read_excel("~/Malaria Data Files/MT_GT_NP.xlsx")
+G = as.matrix(MT_GT_NP)
+
+n = sample(1:333,1)
+plot(log10(M[,n]),type="l",xlim=c(0,300),ylim=c(0,5),xlab="Days",ylab="log10 Parasite Density per microliter")
+lines(log10(G[,n]),type="l",xlim=c(0,300),ylim=c(0,5),col="red")
+title(main="Example Infection")
+
+M[which(M==0)] = NaN
+rm = rowMeans(M,na.rm=T)
+
+##
+##
+##
+####daily plot of parasite densities
+plot(log10(rm),type="l",xlim=c(0,365),xlab="Days Since First Detectable",ylab="log10 Parasites per microL",ylim=c(0,5))
+#abline(h=log10(88))
+title(main="Daily Mean Parasite Densities")
+#abline(h=log10(5),lty=2)
+
+G[which(G==0)] = NaN
+rmg = rowMeans(G,na.rm=T)
+lines(log10(rmg),col="red")
+
+ccf(rm[4:225],rmg[4:225],lag.max=20,type="correlation")
+plot(log10(rm[1:233]),type="l",ylim=c(0,5))
+lines(log10(rmg[8:240]),col="red")
+
+rms = rm[1:233]
+rmgs = rmg[8:240]
+plot(log10(rms),log10(rmgs),xlab="Lagged Asexuals",ylab="Gametocytes")
+
+#hist(log10(M[100,]))
+hist(log10(colMeans(M[1:7,],na.rm=T)),breaks=10,xlim=c(0,5.5))
+hist(log10(colMeans(M[30:36,],na.rm=T)),breaks=10,xlim=c(0,5.5))
+hist(log10(colMeans(M[70:76,],na.rm=T)),breaks=10,xlim=c(0,5.5))
+hist(log10(colMeans(M[110:116,],na.rm=T)),breaks=10,xlim=c(0,5.5))
+hist(log10(colMeans(M[160:166,],na.rm=T)),breaks=10,xlim=c(0,5.5))
+hist(log10(colMeans(M[200:206,],na.rm=T)),breaks=8,xlim=c(0,5.5))
+
+MV = matrix(0,nrow=30,ncol=length(M[1,]))
+for(i in 1:30){
+  MV[i,] = colMeans(M[(30*(i-1)+1):(30*i),],na.rm=T)
+}
+
+MV[which(MV==0)] = NaN
+
+vioplot(log10(na.omit(MV[1,])),log10(na.omit(MV[2,])),log10(na.omit(MV[3,])),log10(na.omit(MV[4,])),log10(na.omit(MV[5,])),log10(na.omit(MV[6,])),log10(na.omit(MV[7,])),
+        ylim=c(0,5.2),col="lightblue")
+title(main = "Violin Plot of Monthly Mean Patent Asexual Parasite Densities", xlab="Months Post-Infection",ylab="Mean log10 Parasite Density per microliter")
+abline(h=log10(88))
+abline(h=log10(8),lty=2)
+
+GV = matrix(0,nrow=30,ncol=length(G[1,]))
+for(i in 1:30){
+  GV[i,] = colMeans(G[(30*(i-1)+1):(30*i),],na.rm=T)
+}
+
+GV[which(GV==0)] = NaN
+
+vioplot(log10(na.omit(GV[1,])),log10(na.omit(GV[2,])),log10(na.omit(GV[3,])),log10(na.omit(GV[4,])),log10(na.omit(GV[5,])),log10(na.omit(GV[6,])),log10(na.omit(GV[7,])),
+        ylim=c(0,5.2),col="lightblue")
+title(main = "Violin Plot of Monthly Mean Gametocyte Densities", xlab="Months Post-Infection",ylab="Mean log10 Gametocyte Density per microliter")
+abline(h=log10(88))
+abline(h=log10(8),lty=2)
+
+mu = rep(0,30)
+N = rep(0,30)
+for(i in 1:29){
+  mu[i] = log10(mean(colMeans(M[((i-1)*7+1):(i*7),],na.rm=T),na.rm=T))
+  #N[i] = sum(colMeans(M[((i-1)*7+1):(i*7),],na.rm=T)>0,na.rm=T)
+}
+N = rep(0,333)
+for(i in 1:333){
+  position = which(!is.na(M[,i]))
+  nonzero = which(M[,i]>0)
+  N[i] = max(intersect(position,nonzero))
+}
+mu[30] = log10(mean(colMeans(M[((29)*7+1):nrow(M),],na.rm=T),na.rm=T))
+#N[30] = sum(colMeans(M[((29)*7+1):nrow(M),],na.rm=T)>0,na.rm=T)
+
+N = sort(N)
+
+Nweeks = rep(0,59)
+for(i in 1:59){
+  Nweeks[i] = tail(cumsum(N<(7*i)),1)
+}
+Nweeks = Nweeks/333
+plot(log(1-Nweeks),type="s")
+
+y = log(1-Nweeks)[1:50]
+x = 1:length(y)
+surv = lm(y~x+0)
+summary(surv)
+lambda = -surv$coefficients[[1]]
+
+plot(1:length(y),y)
+lines(seq(0,length(y),.1),-lambda*seq(0,length(y),.1))
+
+plot(1:length(Nweeks),1-Nweeks,xlab="Weeks",ylab="Proportion of Persisting Infections")
+lines(seq(0,length(Nweeks),.1),exp(-lambda*seq(0,length(Nweeks),.1)))
+
+CDFsurv = function(x){
+  1-exp(-lambda*x)
+}
+
+plot(1:length(Nweeks),Nweeks,type="s")
+
+t = 0:100
+lines(t,CDFsurv(t))
+
+data = Nweeks
+ks.test(data,CDFsurv)
+
+hist(N,breaks=30)
+
+s = rep(0,30)
+for(i in 1:29){
+  s[i] = log10(var(colMeans(M[((i-1)*7+1):(i*7),],na.rm=T),na.rm=T))
+}
+s[30] = log10(var(colMeans(M[(29*7+1):nrow(M),],na.rm=T),na.rm=T))
+
+f = splinefun(mu,method="fmm")
+plot(0:29,mu,ylim=c(0,5),xlab="Weeks Since First Detection",ylab="log10 Parasites / microliter")
+lines(seq(0,29,.01),f(seq(1,30,.01)),lty=2)
+abline(h=log10(88))
+
+var = s
+lines(0:29,sqrt(var),lty=2)
+
+fit = lm(var~mu)
+lin = function(x){
+  fit$coefficients[[1]] + fit$coefficients[[2]]*x
+}
+
+plot(mu,var,xlab="Mean of log10 Parasites per microliter",ylab="Variance of log10 Parasites per microliter")
+x = seq(0,5,.01)
+lines(x,lin(x))
+abline(v=log10(88),lty=2)
+title(main="Mean-Variance Power Law for Asexual Parasitemia")
+
+
+## examination of residuals
+resid = var-lin(mu)
+plot(resid)
+
+me = mean(resid)
+se = var(resid)
+residNorm = (resid-me)/sqrt(se)
+hist(residNorm,breaks=50)
+qqnorm(y=residNorm)
+lines(seq(-2,2,.01),seq(-2,2,.01))
+
+
+#### gametocytes
+
+gmu = rep(0,30)
+for(i in 1:29){
+  gmu[i] = log10(mean(colMeans(G[((i-1)*7+1):(i*7),],na.rm=T),na.rm=T))
+}
+gmu[30] = log10(mean(colMeans(G[(29*7+1):nrow(G),],na.rm=T),na.rm=T))
+
+g = splinefun(gmu,method="fmm")
+plot(0:29,gmu,ylim=c(-.2,5),xlab="Weeks Since First Detection",ylab="Mean log10 Parasites / microliter")
+lines(seq(0,29,.01),g(seq(1,30,.01)),lty=2,col="red")
+points(0:29,mu)
+lines(seq(0,29,.01),f(seq(1,30,.01)),lty=2)
+title(main="Mean Parasitemia Profile, Conditioned on Persistence")
+
+
+gs = rep(0,30)
+for(i in 1:30){
+  gs[i] = log10(var(colMeans(G[((i-1)*7+1):(i*7),],na.rm=T),na.rm=T))
+}
+
+gfit = lm(gs~gmu)
+glin = function(x){
+  gfit$coefficients[[1]]+gfit$coefficients[[2]]*x
+}
+plot(gmu,gs,xlab = "log10 Mean Gametocytemia", ylab = "log10 Variance of Gametocytemia")
+lines(gmu,glin(gmu))
+title(main="Mean-Variance Power Law for Gametocytemia")
+
+plot(gfit$residuals)
+gresidNorm = (gfit$residuals-mean(gfit$residuals))/(sqrt(var(gfit$residuals)))
+qqnorm(gresidNorm)
+lines(seq(-2,2,.1),seq(-2,2,.1))
+
+## check for cross-correlation in mu, gmu - best correlation at a 1 week lag
+ccf(mu,gmu,type="correlation",main="")
+title(main="Cross-Correlation Between Asexual Parasitemia and Gametocytemia")
+
+pg = lm(gmu[2:30]~mu[1:29])
+pgline = function(x){
+  pg$coefficients[1]+pg$coefficients[[2]]*x
+}
+plot(mu[1:29],gmu[2:30],xlab="Mean log10 Parasites per microliter", ylab="One Week Lagged Mean log10 Gametocytes per microliter")
+lines(mu[1:29],pgline(mu[1:29]))
+title(main="Mean Asexual Parasitemia vs Lagged Gametocytemia")
+
+plot(10^mu[1:29],10^gmu[2:30],xlab="mean asexual densities",ylab="lagged mean gametocyte densities")
+lines(10^seq(1,5,.01),.047*10^(1.048*seq(1,5,.01)))
+
+
+
+############################################################ ACTUAL power laws for mean/variance
+
+
+
+rv = rep(0,dim(M)[1])
+for(i in 1:dim(M)[1]){
+  rv[i] = var(M[i,],na.rm=T)
+}
+rm = rowMeans(M,na.rm=T)
+plot(rv,type="l",xlim=c(0,500),xlab="Days Since First Detectable",ylab="log10 Parasites per microL")
+title(main="Daily Variance of Parasite Densities")
+
+rv[which(rv==0)]=NaN
+rm[which(rm==0)]=NaN
+rm = log10(rm)
+rv = log10(rv)
+mv = lm(rv~rm)
+fmv = function(x){
+  mv$coefficients[[1]]+mv$coefficients[[2]]*x
+}
+###
+###
+###
+#### Mean/Variance relationship for Asexual parasitemia
+plot(rm,rv,xlab="log10 Mean Parasite Density",ylab="log10 Variance of Parasite Density")
+#lines(seq(-1,10,.01),fmv(seq(-1,10,.01)),col="blue")
+title(main="Mean-Variance Power Law for Asexual Parasites")
+lines(seq(1,5,.01),fmv(seq(1,5,.01)))
+
+plot(mv$residuals,xlab="Days",ylab="Residual")
+title(main="Plot of Residuals from Asexual Mean-Variance Power Law")
+hist(mv$residuals,xlab="Residual",main="")
+title("Histogram of Residuals from Asexual Mean-Variance Power Law")
+qqnorm((mv$residuals-mean(mv$residuals))/sqrt(var(mv$residuals)))
+lines(seq(-4,4,.1),seq(-4,4,.1))
+
+grv = rep(0,dim(G)[1])
+for(i in 1:dim(G)[1]){
+  grv[i] = var(G[i,],na.rm=T)
+}
+grm = rowMeans(G,na.rm=T)
+plot(log10(grv),type="l",xlim=c(0,500),xlab="Days Since First Detectable",ylab="log10 Gametocytes per microL")
+abline(h=log10(88))
+title(main="Daily Variance of Gametocyte Densities")
+abline(h=log10(5),lty=2)
+
+grv[which(grv==0)]=NaN
+grm[which(grm==0)]=NaN
+gmv = lm(log10(grv)~log10(grm))
+
+ggmv = function(x){
+  gmv$coefficients[[1]]+gmv$coefficients[[2]]*x
+}
+plot(log10(grm),log10(grv),xlab="log10 Mean Gametocyte Density",ylab="log10 Variance of Gametocyte Density")
+lines(seq(-1,10,.01),ggmv(seq(-1,10,.01)),col="blue")
+title(main="Mean-Variance Power Law for Gametocytes")
+
+plot(gmv$residuals)
+title(main="Plot of Residuals from Gametocyte Mean-Variance Power Law")
+hist(gmv$residuals,main="")
+title("Histogram of Residuals from Gametocyte Mean-Variance Power Law")
+qqnorm((gmv$residuals-mean(gmv$residuals))/sqrt(var(gmv$residuals)))
+lines(seq(-4,4,.1),seq(-4,4,.1))
+
+ccf(rm,grm,na.action = na.contiguous,main="Cross-Correlation between Asexual and Gametocyte Densities",ylab="CCF")
+plot(log10(rm)[1:(length(rm)-8)],log10(grm)[9:length(rm)],xlab="Lagged log10 Mean Asexual Parasite Densities per microL",ylab="log10 Mean Gametocyte Densities",main="Lagged Gametocyte Production")
+rmd = log10(rm)[1:(length(rm)-8)]
+rmd[which(is.infinite(rmd))] = NaN
+grmd = log10(grm)[9:length(rm)]
+grmd[which(is.infinite(grmd))] = NaN
+ptgt = lm(grmd~rmd)
+fg = function(x){
+  ptgt$coefficients[[1]]+x*ptgt$coefficients[[2]]
+}
+lines(seq(-1,5,.01),fg(seq(-1,5,.01)))
+
+##restrict to larger than ~88 parasites per microliter for both gametocytes and asexuals
+
+rmdp = rmd[which(grmd>2.3)]
+grmdp = grmd[which(grmd>2.3)]
+plot(rmdp,grmdp,xlab="8 Day Lagged log10 Mean Asexual Parasite Densities",ylab="log10 Mean Gametocyte Densities")
+title(main="Lagged Power Law Relationship")
+ptgtp = lm(grmdp~rmdp)
+fgp = function(x){
+  ptgtp$coefficients[[1]]+x*ptgtp$coefficients[[2]]
+}
+lines(seq(-1,5,.01),fgp(seq(-1,5,.01)))
+
+plot(ptgtp$residuals)
+hist(ptgtp$residuals,breaks=10)
+
+qqnorm((ptgtp$residuals-mean(ptgtp$residuals))/sqrt(var(ptgtp$residuals)))
+lines(seq(-5,5,.01),seq(-5,5,.01))
+
+##uses restricted dataset
+rmpp = c(0,0,0,0,0,0,0,0,rm)
+plot(log10(rm),type="l",ylim=c(-1,5),xlim=c(0,230),xlab="Days",ylab="log10 Density per microliter")
+lines(ptgtp$coefficients[[1]]+log10(rmpp)*ptgtp$coefficients[[2]],col="blue")
+lines(log10(grm),col="red")
+title(main="Measured vs Fitted Gametocytes with Asexual Predictor")
+
+##uses full dataset
+rmpp = c(0,0,0,0,0,0,0,0,rm)
+plot(log10(rm),type="l",ylim=c(-1,5),xlim=c(0,230),xlab="Days",ylab="log10 Density per microliter")
+lines(ptgt$coefficients[[1]]+log10(rmpp)*ptgt$coefficients[[2]],col="blue")
+lines(log10(grm),col="red")
+title(main="Measured vs Fitted Gametocytes with Asexual Predictor")
+
+
+
+
+
+
+################## piecewise-linear fitting of the two previous models
+
+fgpl = function(x){
+    x1 = x[which(x>=3.3)]
+    x2 = x[which(x<3.3)]
+    y1 = ptgtp$coefficients[[1]]+x1*ptgtp$coefficients[[2]]
+    y2 = ptgt$coefficients[[1]]+x2*ptgt$coefficients[[2]]
+    y = rep(0,length(x))
+    y[which(x>=3.3)] = y1
+    y[which(x<3.3)] = y2
+    return(y)
+}
+
+plot(rmd,grmd,xlab="9 Day Lagged log10 Mean Asexual Parasite Densities",ylab="log10 Mean Gametocyte Densities")
+title(main="Lagged Power Law Relationship")
+lines(seq(-1,5,.01),fgpl(seq(-1,5,.01)))
+
+##see how it all fits together
+plot(log10(rm),type="l",ylim=c(0,5),xlim=c(0,250),xlab="Days",ylab="log10 Density per microliter")
+lines(fgpl(log10(rmpp)),col="blue",xlab="Days",ylab="log10 Density per microliter",type="l",ylim=c(-1,3.5),xlim=c(0,240))
+lines(log10(grm),col="red")
+title(main="Measured vs Fitted Gametocytes with Asexual Predictor")
+
+presid = log10(grm[9:length(grm)])-fgpl(log10(rm[1:(length(rm)-8)]))
+presid = presid[!is.na(presid)]
+plot(presid)
+hist(presid,freq=F,breaks=20)
+qqnorm((presid-mean(presid))/sqrt(var(presid)))
+lines(seq(-4,4,.01),seq(-4,4,.01))
+
+plot(10^fgpl(log10(rmpp)),col="blue",xlab="Days",ylab="Density per microliter",type="l",xlim=c(0,240),ylim=c(0,2500))
+lines(grm,col="red")
+title(main="Measured vs Fitted Gametocytes with Asexual Predictor")
+
+
+
+####################################### Tent Parameters ######################################
+
+# these two are calculated already
+firstPatent
+lastPatent
+#these two need declaration + computation
+peak = rep(0,333)
+peakT = rep(0,333)
+
+for(i in 1:333){
+  peak[i] = max(M[,i],na.rm=T)
+  peakT[i] = which(M[,i]==peak[i])[1]-firstPatent[i]
+}
+
+hist(log10(peak),breaks=30,freq=F,xlim=c(2,6),ylim=c(0,1.2))
+z = seq(2,6,.01)
+lines(z,dnorm(z,mean(log10(peak)),var(log10(peak))+.15))
+
+hist(peakT,breaks=30,freq=F,ylim=c(0,.2))
+z = seq(0,80)
+lines(z,dpois(z,mean(peakT)-3))
+
+plot(peakT,log10(peak))
+plot(lastPatent,log10(peak))
+
+
+################################## Test for Recrudescence ####################################
+
+
+plot(log10(rm),xlim=c(0,300),type="l",ylim=c(-1,6))
+upper = rm + 3*sqrt(rv)
+lines(log10(upper))
+
+old = rep(0,1326)
+for(i in 1:333){
+  for(j in 1:1326){
+    if(M[j,i])
+    if(M[j,i] > upper[j]){
+      old[j] = old[j]+1
+    }
+  }
+}
+
+##################################### Individual Variation ###################################
+######################## linear fit from peak to end, FFT on residuals #######################
+
+## find maximum over every individual infection
+Mmax = rep(NaN,333)
+## find the position (day) maximum occurs
+pMmax = Mmax
+## day of first patency
+firstPatent = Mmax
+## day of last patency
+lastPatent = Mmax
+
+for(i in 1:333){
+  Mmax[i] = max(M[,i],na.rm=T)
+  firstPatent[i] = min(which(M[,i] > 0))
+  pMmax[i] = min(which(M[,i] == Mmax[i]))-firstPatent[i]+1
+  lastPatent[i] = max(which(M[,i]>0))-firstPatent[i]+1
+}
+
+hist(pMmax,breaks=50,freq=F,ylim=c(0,.18))
+f = function(a,t){a/sqrt(2*pi*t^3)*exp(-(a-t)^2/(2*t))}
+d = 1:80
+lines(f(7,d))
+hist(log10(Mmax),xlim=c(2,7),breaks=20)
+hist(lastPatent,breaks=20,xlab="Duration of Infection (in Days)")
+
+#### run linear fit from peak to end of every infection ####
+
+intercept = rep(0,333)
+slope = rep(0,333)
+for(i in 1:333){
+  x = pMmax[i]:lastPatent[i]
+  y = log10(M[pMmax[i]:lastPatent[i],i])
+  model = lm(y~x)
+  intercept[i] = model$coefficients[[1]]
+  slope[i] = model$coefficients[[2]]
+}
+
+## these 2 have undefined slope
+which(is.na(slope))
+## these 8 had a positive slope
+which(slope>0)
+
+LF = NaN*M
+for(i in 1:333){
+  for(j in pMmax[i]:lastPatent[i]){
+    LF[j,i] = intercept[i]+j*slope[i]
+  }
+}
+
+R = log10(M)-LF
+plot(R[,2],xlim=c(1,60))
+
+## define Rint, the linearly interpolated R for missing data between the beginning and end
+Rint = list()
+for(i in 1:333){
+  t = pMmax[i]:lastPatent[i]
+  Rint[[i]] = na.stinterp(R[t,i])
+}
+
+Rtrunc = list()
+for(i in 1:333){
+  trunc = min(which(is.na(R[1:length(R[,i]),i])))
+  Rtrunc[[i]] = R[pMmax[i]:trunc,i]
+}
+
+plot(Rtrunc[[7]])
+
+patient = list()
+j = 1
+for(i in 1:333){
+  if(length(Rtrunc[[i]]) >= 50){
+    patient[[j]] = i
+    j = j+1
+  }
+}
+
+RRtrunc = list()
+for(i in 1:length(patient)){
+  RRtrunc[[i]] = abs(fft(Rtrunc[[patient[[i]]]]))
+}
+
+n = 10
+plot(seq(0,1-1/length(Rint[[n]]),1/length(Rint[[n]])),abs(fft(Rint[[n]])))
+
+
+#############################
+n = 19
+plot(M[1:100,n],xlim=c(0,100),type="l")
+plot(log10(M[1:100,n]),xlim=c(0,100),type="l")
+lines(intercept[n]+seq(0,100)*slope[n])
+plot(R[1:100,n],xlim=c(0,100),type="l",xlab="Days")
+
+plot(na.stinterp(R[1:100,n]),xlim=c(0,100),type="l",xlab="Days")
+
+#acf(na.stinterp(R[1:100,n]))
+#pacf(na.stinterp(R[1:100,n]))
+
+N = length(na.stinterp(R[1:100,n]))
+freq = 1:N/N
+
+plot(freq,abs(fft(na.stinterp(R[1:100,n]))),xlim=c(0,.5),ylab="A",type="l",xlab="cycles per day")
+m = which(abs(fft(na.stinterp(R[1:100,n])))==max(abs(fft(na.stinterp(R[1:100,n])))))
+1/freq[m][1]
+abline(v=freq[m][1])
+abline(v=.094)
+abline(v=.031)
+length(na.stinterp(R[1:100,n]))
+
+## differenced data 
+plot(diff(na.stinterp(R[1:150,n])))
+acf(diff(na.stinterp(R[1:150,n])))
+pacf(diff(na.stinterp(R[,n])))
+
+
+################
+
--- MASH-dev/JohnHenry/Pv_Analysis/Pv_Data_Analysis.R
+++ MASH-dev/JohnHenry/Pv_Analysis/Pv_Data_Analysis.R
@@ -56,10 +56,10 @@ lines(x,dexp(x,.01485))
 ## this should be a straight line if exponentially distributed with slope = 1/mean
 ## not enough infections end very early on
 #plot(-log(1-cumsum(rev(sort(PvDur)))/sum(PvDur)))
-
+x = seq(0,400)
 hist(PvDur,breaks=25,freq=F,xlab="Days",main="Duration of Pv Infection",ylim=c(0,.02))
 #hist(PvDur,breaks=25,freq=F,ylim=c(0,.02))
-shift = 10
+shift = 14
 ## maybe try sinusoidally varying intensity with period of ~90 days, shifted slightly
 lines(x,.015*(sin(pi*(x-shift)/45)+1)*exp(-.015*(45/pi*cos(pi*(x-shift)/45)+x)),type="l")
 
@@ -80,10 +80,10 @@ pdf = diff(Durcdf(x))
 plot(seq(1,155),pdf,type="l")
 cdf = Durcdf(1:155)
 mu = mean(pdf/(1-cdf))
-plot(pdf/(1-cdf)-mu,ylim=c(-.1,.14),type="l")
+plot(pdf/(1-cdf)-mu,ylim=c(-.1,.14))
 xf = seq(0,154)/155
 plot(xf,abs(fft(pdf/(1-cdf)-mu)),xlim=c(0,.2))
-plot(pdf/(1-cdf)-mu,ylim=c(-.1,.14),type="l")
+plot(pdf/(1-cdf)-mu,ylim=c(-.1,.14))
 lines(x,.015*sin(pi*(x-20)/45))
 
 ################### Asexual Parasitemia ##########################
@@ -111,3 +111,101 @@ lines(seq(0,5,.1),seq(0,5,.1)*m+b)
 
 ######################## Gametocytemia #############################
 
+
+split = rep(0,335)
+j = 1
+for(i in 1:35199){
+  if(Vivax$meta_patient_id[i]!=Vivax$meta_patient_id[i+1]){
+    split[j] = i
+    j = j+1
+  }
+}
+split = c(0,split)
+M = max(abs(diff(split)))
+VG = matrix(nrow=336,ncol=M)
+
+for(i in 1:335){
+  temp = Vivax$gametocytes_f[(split[i]+1):split[i+1]]+Vivax$gametocytes_m[(split[i]+1):split[i+1]]
+  pad = rep(NaN,M-length(temp))
+  temp = c(temp,pad)
+  VG[i,] = temp
+}
+
+MVG = log10(rowMeans(t(VG),na.rm=T))
+plot(MVP,type="l")
+lines(MVG,lty=2)
+
+lag = 7
+plot(MVP[(lag+1):400],MVG[1:(400-lag)])
+
+ccf = rep(0,15)
+for(i in 1:15){
+  
+  dh = i-1
+  both = which((!is.na(c(rep(0,dh),MVP)))&is.finite(c(rep(0,dh),MVP))&is.finite(c(MVG,rep(NaN,dh)))&(!is.na(c(MVG,rep(NaN,dh)))))
+  MVPb = MVP[both]
+  MVGb = MVG[both]
+  ccf[i] = cor(MVPb,MVGb)
+  
+}
+
+plot(0:14,ccf,xlab="Delay (in Days)",ylab="correlation")
+
+VVG = log10(rowVars(t(VG)))
+plot(MVG,VVG)
+
+MVG[which(is.infinite(MVG))] = NaN
+MVP[which(is.infinite(MVP))] = NaN
+ccf(MVP, MVG, na.action = na.contiguous)
+acf(MVP, na.action=na.contiguous,main="ACF for Mean Asexual P. vivax Densities")
+pacf(MVP,na.action=na.contiguous,main="PACF for Mean Asexual P. vivax Densities")
+
+
+#################### Transmission Efficiency
+
+
+split = rep(0,335)
+j = 1
+for(i in 1:35199){
+  if(Vivax$meta_patient_id[i]!=Vivax$meta_patient_id[i+1]){
+    split[j] = i
+    j = j+1
+  }
+}
+split = c(0,split)
+M = max(abs(diff(split)))
+VTE = matrix(nrow=336,ncol=M)
+
+for(i in 1:335){
+  temp = Vivax$mosquitoes[(split[i]+1):split[i+1]]
+  pad = rep(NaN,M-length(temp))
+  temp = c(temp,pad)
+  VTE[i,] = temp
+}
+VTE[which(VTE==max(Vivax$mosquitoes,na.rm=T))] = NaN
+MVTE = rowMeans(t(VTE),na.rm=T)/100
+plot(MVG,type="l")
+lines(MVTE,lty=2)
+
+plot(MVG,MVTE)
+
+smoothedTE = function(x){
+  y = rep(0,length(x))
+  for(i in 1:length(x)){
+    y[i] = mean(MVTE[which(MVG>=x[i] & MVG < (x[i]+.5))],na.rm=T)
+  }
+  return(y)
+}
+
+z = seq(0,2.5,.1)
+y = smoothedTE(z)
+plot(z+.25,y,xlab="log10 Gametocyte Density",ylab="Transmission Efficiency",ylim=c(0,1))
+
+sigfit = nls(y~p1*exp(p2*(z+.25))/(p3+exp(p2*(z+.25))),start=list(p1=1,p2=1,p3=.5))
+p1=.5003
+p2=.8535
+p3=.9324
+lines(z+.25, p1*exp(p2*(z+.25))/(p3+exp(p2*(z+.25))))
+
+VVTE = rowVars(t(VTE),na.rm=T)
+plot(MVTE,VVTE)
