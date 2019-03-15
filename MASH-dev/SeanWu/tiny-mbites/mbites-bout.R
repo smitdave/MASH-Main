9ba097d83bf7f5de13eb11e8dcc0cfba3f6cb0a7
slwu89 <slwu89@berkeley.edu>
2019-03-14 19:23:42 -0700

    more progress on tiny-mbites

--- MASH-dev/SeanWu/tiny-mbites/mbites-bout.R
+++ MASH-dev/SeanWu/tiny-mbites/mbites-bout.R
@@ -1,19 +1,39 @@
 # main simulation loop for ABM
 # tnow = get("globals",.GlobalEnv)$get_tnow()
 MBITES <- function(mosy,tnow){
-
-  # dont simulate on dead mosquitos
+  # the main simulation loop
   while(mosy$tnext < tnow & mosy$statenext != "D"){
 
+    # enter the new state; update my local time and state
     mosy$tnow <- mosy$tnext
     mosy$state <- mosy$statenext
 
+    switch(mosy$state,
+      F = {activity_BFSB(mosy)},
+      B = {activity_BFAB(mosy)},
+      L = {activity_ELSB(mosy)},
+      O = {activity_ELAB(mosy)},
+      # M = {self$activity_MAB()},
+      # S = {self$activity_SFAB()},
+      {stop("mosquito ",mosy$id," calling illegal behavioral state: ",mosy$state,"\n")}
+    )
 
+    # land and rest
+    restingspot(mosy)
 
+    # energetics
+    energetics(mosy)
+
+    # survival
+    survival(mosy)
+
+    # oogenesis & refeeding
+    oogenesis(mosy)
+
+    # timing
+    timing(mosy)
+
+    # 'just before' i jump out of this state (including to the dead state)
+    track_history(mosy)
   }
-
-  if(mosy$statenext == "D"){
-    # have the mosquito log itself tag for deletion/replacement
-  }
-
 }
---
+++ MASH-dev/SeanWu/tiny-mbites/parameters.R
@@ -0,0 +1,15 @@
+# parameters
+make_parameters <- function(
+  # timing
+  time_BFSB = 1/(6/24),
+  time_BFAB = 1/(3/24),
+  time_ELSB = 1/(6/24),
+  time_ELAB = 1/(3/24),
+  time_ppr = 1/(18/24),
+){
+
+  out <- list()
+
+
+  list2env(out,hash=TRUE)
+}
