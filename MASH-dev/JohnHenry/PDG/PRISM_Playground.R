dc75402088c4ce3424d30477af003a281a65f05f
jmh227 <henry529@uw.edu>
2019-04-08 13:24:05 -0700

    Model Fit for hemoglobin by age and infection status

--- MASH-dev/JohnHenry/PDG/PRISM_Playground.R
+++ MASH-dev/JohnHenry/PDG/PRISM_Playground.R
@@ -12,7 +12,7 @@ max(Hb,na.rm=T)
 Hb[which(Hb>25)] = NaN
 
 
-plot(log10(PD),Hb,ylim=c(0,20),xlab="Parasite Denisty",ylab="Hemoglobin Reading")
+plot(log10(PD),Hb,ylim=c(5,15),xlab="Parasite Denisty",ylab="Hemoglobin Reading")
 abline(h=12)
 abline(h=10)
 abline(h=7)
@@ -192,6 +192,14 @@ healthyFemaleHbmu = rep(NaN,(12*8+1))
 healthyMaleHbmu = rep(NaN,(12*8+1))
 infectedFemaleHbmu = rep(NaN,(12*8+1))
 infectedMaleHbmu = rep(NaN,(12*8+1))
+healthyFemaleHbvar = rep(NaN,(12*8+1))
+healthyMaleHbvar = rep(NaN,(12*8+1))
+infectedFemaleHbvar = rep(NaN,(12*8+1))
+infectedMaleHbvar = rep(NaN,(12*8+1))
+healthyFemaleHbN = rep(NaN,(12*8+1))
+healthyMaleHbN = rep(NaN,(12*8+1))
+infectedFemaleHbN = rep(NaN,(12*8+1))
+infectedMaleHbN = rep(NaN,(12*8+1))
 for(i in 1:(12*8+1)){
   agemin = (i-1)/12
   agemax = agemin+1/12
@@ -199,16 +207,28 @@ for(i in 1:(12*8+1)){
   healthyMaleHbmu[i] = mean(healthyMaleHb[which(healthyMaleAge>agemin & healthyMaleAge<=agemax)],na.rm=T)
   infectedFemaleHbmu[i] = mean(infectedFemaleHb[which(infectedFemaleAge>agemin & infectedFemaleAge<=agemax)],na.rm=T)
   infectedMaleHbmu[i] = mean(infectedMaleHb[which(infectedMaleAge>agemin & infectedMaleAge<=agemax)],na.rm=T)
+  healthyFemaleHbvar[i] = var(healthyFemaleHb[which(healthyFemaleAge>agemin & healthyFemaleAge<=agemax)],na.rm=T)
+  healthyMaleHbvar[i] = var(healthyMaleHb[which(healthyMaleAge>agemin & healthyMaleAge<=agemax)],na.rm=T)
+  infectedFemaleHbvar[i] = var(infectedFemaleHb[which(infectedFemaleAge>agemin & infectedFemaleAge<=agemax)],na.rm=T)
+  infectedMaleHbvar[i] = var(infectedMaleHb[which(infectedMaleAge>agemin & infectedMaleAge<=agemax)],na.rm=T)
+  healthyFemaleHbN[i] = sum((healthyFemaleHb[which(healthyFemaleAge>agemin & healthyFemaleAge<=agemax)]>0),na.rm=T)
+  healthyMaleHbN[i] = sum((healthyMaleHb[which(healthyMaleAge>agemin & healthyMaleAge<=agemax)]>0),na.rm=T)
+  infectedFemaleHbN[i] = sum((infectedFemaleHb[which(infectedFemaleAge>agemin & infectedFemaleAge<=agemax)]>0),na.rm=T)
+  infectedMaleHbN[i] = sum((infectedMaleHb[which(infectedMaleAge>agemin & infectedMaleAge<=agemax)]>0),na.rm=T)
 }
 age = seq(0,8,1/12)
 
+######### Compare Means
 par(mfrow=c(1,2))
 plot(age,healthyFemaleHbmu,type="l",ylim=c(9,13),ylab="Mean Hb Concentration (g/dl)",xlab="age (in years)",main="Mean Hb Healthy Vs Infected Females")
 lines(age,infectedFemaleHbmu,col="red")
+abline(h=c(11,10,7),lty=2)
 plot(age,healthyMaleHbmu,type="l",ylim=c(9,13),ylab="Mean Hb Concentration (g/dl)",xlab="age (in years)",main="Mean Hb Healthy Vs Infected Males")
+abline(h=c(11,10,7),lty=2)
 lines(age,infectedMaleHbmu,type="l",col="red")
 par(mfrow=c(1,1))
 
+######### Mean Differences
 par(mfrow=c(1,2))
 plot(age,healthyFemaleHbmu-infectedFemaleHbmu,type="l",main="Difference in Female Mean Hb by Infection Status",xlab="age",ylab="Hb Concentration, g/dl",ylim=c(-2,3))
 abline(h=mean(healthyFemaleHbmu-infectedFemaleHbmu,na.rm=T))
@@ -216,3 +236,81 @@ plot(age,healthyMaleHbmu-infectedMaleHbmu,type="l",main="Difference in Male Mean
 abline(h=mean(healthyMaleHbmu-infectedMaleHbmu,na.rm=T))
 par(mfrow=c(1,1))
 
+## two sample t-test of differences in mean
+t.test((healthyFemaleHbmu-infectedFemaleHbmu),(healthyMaleHbmu-infectedMaleHbmu))
+## no statistically significant difference in the average decline in Hb by sex
+
+
+######### Compare SDs
+par(mfrow=c(1,2))
+plot(age,sqrt(healthyFemaleHbvar),type="l",ylim=c(0,3),ylab="SD Hb Concentration (g/dl)",xlab="age (in years)",main="SD Hb Healthy Vs Infected Females")
+lines(age,sqrt(infectedFemaleHbvar),col="red")
+plot(age,sqrt(healthyMaleHbvar),type="l",ylim=c(0,3),ylab="SD Hb Concentration (g/dl)",xlab="age (in years)",main="SD Hb Healthy Vs Infected Males")
+lines(age,sqrt(infectedMaleHbvar),type="l",col="red")
+par(mfrow=c(1,1))
+
+######### Compare Sample Sizes
+par(mfrow=c(1,2))
+plot(age,log10(healthyFemaleHbN),type="l",ylim=c(0,3),ylab="Sample Sizes of Hb Concentration (g/dl)",xlab="age (in years)",main="SD Hb Healthy Vs Infected Females")
+lines(age,log10(infectedFemaleHbN),col="red")
+plot(age,log10(healthyMaleHbN),type="l",ylim=c(0,3),ylab="Sample Sizes of Hb Concentration (g/dl)",xlab="age (in years)",main="SD Hb Healthy Vs Infected Males")
+lines(age,log10(infectedMaleHbN),type="l",col="red")
+par(mfrow=c(1,1))
+
+
+####################### Fit model to Hb Levels by age/sex
+
+ageSub = seq(.5,8,1/12)
+
+mf = function(age,Hb0,a,b){
+  a/b*(1-exp(-b*age))+Hb0*exp(-b*age)
+}
+
+par(mfrow=c(1,2))
+healthyFemaleHbmuSub = healthyFemaleHbmu[which(age>=.5)]
+infectedFemaleHbmuSub = infectedFemaleHbmu[which(age>=.5)]
+healthyMaleHbmuSub = healthyMaleHbmu[which(age>=.5)]
+infectedMaleHbmuSub = infectedMaleHbmu[which(age>=.5)]
+
+healthyFemaleHbNSub = healthyFemaleHbN[which(age>=.5)]
+infectedFemaleHbNSub = infectedFemaleHbN[which(age>=.5)]
+healthyMaleHbNSub = healthyMaleHbN[which(age>=.5)]
+infectedMaleHbNSub = infectedMaleHbN[which(age>=.5)]
+
+nls(healthyFemaleHbmuSub~a/b*(1-exp(-b*ageSub))+Hb0*exp(-b*ageSub),start=list(a=1,b=.2,Hb0=9),weights = sqrt(healthyFemaleHbNSub))
+plot(age,healthyFemaleHbmu,type="l",ylim=c(9,13),ylab="Mean Hb Concentration (g/dl)",xlab="age (in years)",main="Mean Hb Healthy Vs Infected Females")
+aHF = 4.8788
+bHF = .4157
+Hb0HF = 9.7521
+lines(ageSub,mf(ageSub,Hb0HF,aHF,bHF))
+lines(age,infectedFemaleHbmu,col="red")
+nls(infectedFemaleHbmuSub~a/b*(1-exp(-b*ageSub))+Hb0*exp(-b*ageSub),start=list(a=4,b=.4,Hb0=7.5),weights = sqrt(infectedFemaleHbNSub))
+aIF = 6.9462
+bIF = .6469
+Hb0IF = 8.5157
+lines(ageSub,mf(ageSub,Hb0IF,aIF,bIF),col="red")
+
+nls(healthyMaleHbmuSub~a/b*(1-exp(-b*ageSub))+Hb0*exp(-b*ageSub),start=list(a=2,b=.2,Hb0=8),weights = sqrt(healthyMaleHbNSub))
+aHM = 4.7587
+bHM = .4055
+Hb0HM = 9.4930
+plot(age,healthyMaleHbmu,type="l",ylim=c(9,13),main="Mean Hb Healthy Vs Infected Males")
+lines(ageSub,mf(ageSub,Hb0HM,aHM,bHM))
+lines(age,infectedMaleHbmu,col="red")
+nls(infectedMaleHbmuSub~a/b*(1-exp(-b*ageSub))+Hb0*exp(-b*ageSub),start=list(a=2,b=.2,Hb0=8),weights = sqrt(infectedMaleHbNSub))
+aIM = 6.9735
+bIM = .6401
+Hb0IM = 8.3541
+lines(ageSub,mf(ageSub,Hb0IM,aIM,bIM),col="red")
+par(mfrow=c(1,1))
+
+
+####################### use model predicting log10 Parasite Density
+####################### from Hb measurements to give average parasite
+####################### density profile by age and sex
+
+
+
+
+################ Idea: Do trajectory analysis like in Isabelle's paper,
+################ seeing how parasite densities & immune history directly impact Hb
