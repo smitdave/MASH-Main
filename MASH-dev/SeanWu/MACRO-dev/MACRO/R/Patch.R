892165932efd8c582ff1a962b0ce112788a902e8
Daniel Citron <dtcitron@uw.edu>
2019-06-27 13:39:54 -0700

    yet another bug

--- MASH-dev/SeanWu/MACRO-dev/MACRO/R/Patch.R
+++ MASH-dev/SeanWu/MACRO-dev/MACRO/R/Patch.R
@@ -35,7 +35,7 @@ patch_conpars <- function(id,move,bWeightZoo,bWeightZootox,reservoir,res_EIR){
   }
 
 
-  if(!all.equal(sum(move),1))){
+  if(!all.equal(sum(move),1)){
     stop("movement vector 'move' must sum to one")
   }
 
